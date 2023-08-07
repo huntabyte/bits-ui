7bc1334d7f7127be6ff57065050ef08a2d0d556f
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-06 21:26:57 -0400

    add avatar primitive

---
+++ src/lib/primitives/avatar/components/AvatarFallback.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { melt } from "@melt-ui/svelte";
+	import { ctx } from "../ctx.js";
+	import type { FallbackProps } from "../types.js";
+
+	type $$Props = FallbackProps;
+
+	const {
+		elements: { fallback }
+	} = ctx.getFallback();
+</script>
+
+<span use:melt={$fallback} {...$$restProps}>
+	<slot />
+</span>
