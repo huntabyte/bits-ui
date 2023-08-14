a9cf4e447d500a4c3c5a2622f657daaf0e98b85a
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-14 10:32:17 -0400

    styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,42 +36,110 @@
 	</Select.Root>
 </div>
 
-<div>
+<div class="flex min-h-[500px] p-12 bg-neutral-800">
 	<Menubar.Root>
 		<Menubar.Menu>
-			<Menubar.Trigger class="trigger">File</Menubar.Trigger>
-			<Menubar.Content class="menu">
-				<Menubar.Item class="item">Single F</Menubar.Item>
-				<Menubar.Item class="item">Double F</Menubar.Item>
-				<Menubar.Item class="item">Triple F</Menubar.Item>
-				<Menubar.Item class="item">Fourth F</Menubar.Item>
+			<Menubar.Trigger
+				class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-emerald-900 transition-colors hover:bg-white/90 data-[highlighted]:outline-none overflow-visible data-[highlighted]:bg-emerald-200 data-[highlighted]:ring-green-400 cursor-default text-sm font-medium leading-none focus:z-30 focus:ring"
+				>File</Menubar.Trigger
+			>
+			<Menubar.Content
+				class="z-10 flex max-h-[300px] min-w-[220px] flex-col shadow-lg shadow-neutral-900/30 rounded-md bg-white p-1 lg:max-h-none"
+			>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Single F</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Double F</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Triple F</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Fourth F</Menubar.Item
+				>
 			</Menubar.Content>
 		</Menubar.Menu>
 		<Menubar.Menu>
-			<Menubar.Trigger class="menu">Edit</Menubar.Trigger>
-			<Menubar.Content class="menu">
-				<Menubar.Item class="item">Single E</Menubar.Item>
-				<Menubar.Item class="item">Double E</Menubar.Item>
-				<Menubar.Item class="item">Triple E</Menubar.Item>
-				<Menubar.Item class="item">Fourth E</Menubar.Item>
+			<Menubar.Trigger
+				class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-emerald-900 transition-colors hover:bg-white/90 data-[highlighted]:outline-none overflow-visible data-[highlighted]:bg-emerald-200 data-[highlighted]:ring-green-400 cursor-default text-sm font-medium leading-none focus:z-30 focus:ring"
+				>Edit</Menubar.Trigger
+			>
+			<Menubar.Content
+				class="z-10 flex max-h-[300px] min-w-[220px] flex-col shadow-lg shadow-neutral-900/30 rounded-md bg-white p-1 lg:max-h-none"
+			>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Single E</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Double E</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Triple E</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Fourth E</Menubar.Item
+				>
 			</Menubar.Content>
 		</Menubar.Menu>
 		<Menubar.Menu>
-			<Menubar.Trigger class="menu">View</Menubar.Trigger>
-			<Menubar.Content class="menu">
-				<Menubar.Item class="item">Single V</Menubar.Item>
-				<Menubar.Item class="item">Double V</Menubar.Item>
-				<Menubar.Item class="item">Triple V</Menubar.Item>
-				<Menubar.Item class="item">Fourth V</Menubar.Item>
+			<Menubar.Trigger
+				class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-emerald-900 transition-colors hover:bg-white/90 data-[highlighted]:outline-none overflow-visible data-[highlighted]:bg-emerald-200 data-[highlighted]:ring-green-400 cursor-default text-sm font-medium leading-none focus:z-30 focus:ring"
+				>View</Menubar.Trigger
+			>
+			<Menubar.Content
+				class="z-10 flex max-h-[300px] min-w-[220px] flex-col shadow-lg shadow-neutral-900/30 rounded-md bg-white p-1 lg:max-h-none"
+			>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Single V</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Double V</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Triple V</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Fourth V</Menubar.Item
+				>
 			</Menubar.Content>
 		</Menubar.Menu>
 		<Menubar.Menu>
-			<Menubar.Trigger class="menu">Help</Menubar.Trigger>
-			<Menubar.Content class="menu">
-				<Menubar.Item class="item">Single H</Menubar.Item>
-				<Menubar.Item class="item">Double H</Menubar.Item>
-				<Menubar.Item class="item">Triple H</Menubar.Item>
-				<Menubar.Item class="item">Fourth H</Menubar.Item>
+			<Menubar.Trigger
+				class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-emerald-900 transition-colors hover:bg-white/90 data-[highlighted]:outline-none overflow-visible data-[highlighted]:bg-emerald-200 data-[highlighted]:ring-green-400 cursor-default text-sm font-medium leading-none focus:z-30 focus:ring"
+				>Help</Menubar.Trigger
+			>
+			<Menubar.Content
+				class="z-10 flex max-h-[300px] min-w-[220px] flex-col shadow-lg shadow-neutral-900/30 rounded-md bg-white p-1 lg:max-h-none"
+			>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Single H</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Double H</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Triple H</Menubar.Item
+				>
+				<Menubar.Item
+					class="relative h-6 min-h-[24px] select-none rounded-sm pl-6 pr-1 z-20 text-emerald-900 outline-none data-[highlighted]:bg-emerald-200 data-[highlighted]:text-green-900 data-[dsiabled]:text-neutral-300 flex items-center text-sm leading-none cursor-default ring-0"
+					>Fourth H</Menubar.Item
+				>
 			</Menubar.Content>
 		</Menubar.Menu>
 	</Menubar.Root>
