b2793cd5ad8e12522caedd752a5abd730f2a6442
Jaeden <ch.schwartze@gmail.com>
2023-08-18 20:17:42 +0200

    feat: init contentlayer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
+	},
+	assetsInclude: ["**/*.md", "**/*.mdx"],
+	server: {
+		fs: {
+			strict: false
+		}
+	},
+	resolve: {
+		alias: [
+			{
+				find: "contentlayer/generated",
+				replacement: fileURLToPath(new URL("./.contentlayer/generated", import.meta.url))
+			}
+		]
 	}
 });
