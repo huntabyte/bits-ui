36d28cee5a375a741a6a185b2bc789ae488ab5c9
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-25 21:11:50 -0400

    update checkbox input

--- src/lib/bits/checkbox/index.ts
+++ src/lib/bits/checkbox/index.ts
@@ -1,5 +1,14 @@
 import Root from "./components/Checkbox.svelte";
 import Indicator from "./components/CheckboxIndicator.svelte";
+import Input from "./components/CheckboxInput.svelte";
 
-export { Root, Indicator, Root as Checkbox, Indicator as CheckboxIndicator };
+export {
+	Root,
+	Indicator,
+	Input,
+	//
+	Root as Checkbox,
+	Indicator as CheckboxIndicator,
+	Input as CheckboxInput
+};
 export * from "./types.js";
