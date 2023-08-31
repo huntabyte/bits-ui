5d4cf8a6b01cfa8aca6a0e41072991cfedee2323
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-31 16:21:11 -0400

    fix: dialog crashing

--- src/components/demos/dialog-demo.svelte
+++ src/components/demos/dialog-demo.svelte
@@ -3,9 +3,10 @@
 	import * as Dialog from "@/components/ui/dialog";
 	import * as Input from "@/components/ui/input";
 	import * as Label from "@/components/ui/label";
+	import * as Sheet from "@/components/ui/sheet";
 </script>
 
-<Dialog.Root>
+<!-- <Dialog.Root>
 	<Dialog.Trigger class={buttonVariants({ variant: "outline" })}>Edit Profile</Dialog.Trigger>
 	<Dialog.Content class="sm:max-w-[425px]">
 		<Dialog.Header>
@@ -28,4 +29,32 @@
 			<Button type="submit">Save changes</Button>
 		</Dialog.Footer>
 	</Dialog.Content>
-</Dialog.Root>
+</Dialog.Root> -->
+<Sheet.Root>
+	<Sheet.Trigger asChild let:builder>
+		<Button builders={[builder]} variant="outline">Open</Button>
+	</Sheet.Trigger>
+	<Sheet.Content side="right">
+		<Sheet.Header>
+			<Sheet.Title>Edit profile</Sheet.Title>
+			<Sheet.Description>
+				Make changes to your profile here. Click save when you're done.
+			</Sheet.Description>
+		</Sheet.Header>
+		<div class="grid gap-4 py-4">
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label.Root for="name" class="text-right">Name</Label.Root>
+				<Input.Root id="name" value="Pedro Duarte" class="col-span-3" />
+			</div>
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label.Root for="username" class="text-right">Username</Label.Root>
+				<Input.Root id="username" value="@peduarte" class="col-span-3" />
+			</div>
+		</div>
+		<Sheet.Footer>
+			<Sheet.Close asChild let:builder>
+				<Button builders={[builder]} type="submit">Save changes</Button>
+			</Sheet.Close>
+		</Sheet.Footer>
+	</Sheet.Content>
+</Sheet.Root>
--- src/components/ui/sheet/sheet-overlay.svelte
+++ src/components/ui/sheet/sheet-overlay.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
-	import { Dialog as SheetPrimitive } from "@/lib";
+	import { Dialog as SheetPrimitive } from "bits-ui";
 	import { cn } from "@/utils";
+	import { fade } from "svelte/transition";
 
 	type $$Props = SheetPrimitive.OverlayProps;
 
 	let className: $$Props["class"] = undefined;
 	export { className as class };
+	export let transition: $$Props["transition"] = fade;
+	export let transitionConfig: $$Props["transitionConfig"] = {
+		duration: 150
+	};
 </script>
 
 <SheetPrimitive.Overlay
-	class={cn(
-		"fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-		className
-	)}
+	{transition}
+	{transitionConfig}
+	class={cn("fixed inset-0 z-50 bg-background/80 backdrop-blur-sm", className)}
 	{...$$restProps}
 />
