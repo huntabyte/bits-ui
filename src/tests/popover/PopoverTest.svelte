15ae40ae571d83465749887bd6809d102060becd
Hunter Johnston <johnstonhuntera@gmail.com>
2023-10-28 22:06:58 -0400

    add popover tests

---
+++ src/tests/popover/PopoverTest.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Popover } from "$lib";
+
+	type $$Props = Popover.Props;
+
+	export let open: $$Props["open"] = false;
+</script>
+
+<main data-testid="main">
+	<Popover.Root bind:open {...$$restProps}>
+		<Popover.Arrow data-testid="arrow" />
+		<Popover.Trigger data-testid="trigger">trigger</Popover.Trigger>
+		<Popover.Content data-testid="content">
+			content
+			<Popover.Close data-testid="close">close</Popover.Close>
+		</Popover.Content>
+	</Popover.Root>
+
+	<button data-testid="binding" on:click={() => (open = !open)}>{open}</button>
+	<div data-testid="outside">outside</div>
+</main>
+<div data-testid="portal-target" id="portal-target" />
--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -5,7 +5,7 @@ import type { Matcher, MatcherOptions } from "@testing-library/svelte";
  * A wrapper around the internal kbd object to make it easier to use in tests
  * which require the key names to be wrapped in curly braces.
  */
-type KbdKeys = keyof typeof kbd;
+export type KbdKeys = keyof typeof kbd;
 export const testKbd: Record<KbdKeys, string> = Object.entries(kbd).reduce((acc, [key, value]) => {
 	acc[key as KbdKeys] = `{${value}}`;
 	return acc;
