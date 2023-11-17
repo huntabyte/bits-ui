de67cfe87f11af080fe78e140d8dcccdb7541c68
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-16 20:42:55 -0500

    wip dialog

--- src/components/demos/alert-dialog-demo.svelte
+++ src/components/demos/alert-dialog-demo.svelte
@@ -1,23 +1,50 @@
 <script lang="ts">
-	import * as AlertDialog from "@/components/ui/alert-dialog";
-	import { Button } from "@/components/ui/button";
+	import { AlertDialog } from "$lib";
+	import { Button } from "$lib";
+	import { flyAndScale } from "@/utils";
+	import { fade } from "svelte/transition";
 </script>
 
 <AlertDialog.Root>
 	<AlertDialog.Trigger asChild let:builder>
-		<Button builders={[builder]} variant="outline">Show Dialog</Button>
+		<Button.Root
+			builders={[builder]}
+			class="rounded-input h-12 text-[15px] font-semibold px-[21px] inline-flex
+		items-center justify-center bg-dark text-background hover:button-gradient
+		shadow-mini transition-all"
+		>
+			Subscribe
+		</Button.Root>
 	</AlertDialog.Trigger>
-	<AlertDialog.Content>
-		<AlertDialog.Header>
-			<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-			<AlertDialog.Description>
-				This action cannot be undone. This will permanently delete your account and remove your data
-				from our servers.
-			</AlertDialog.Description>
-		</AlertDialog.Header>
-		<AlertDialog.Footer>
-			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-			<AlertDialog.Action>Continue</AlertDialog.Action>
-		</AlertDialog.Footer>
-	</AlertDialog.Content>
+	<AlertDialog.Portal>
+		<AlertDialog.Overlay
+			transition={fade}
+			transitionConfig={{ duration: 150 }}
+			class="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm"
+		/>
+		<AlertDialog.Content
+			transition={flyAndScale}
+			class="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-7 rounded-card-lg md:w-full outline-none shadow-popover"
+		>
+			<div class="flex flex-col gap-4 pb-6">
+				<AlertDialog.Title class="font-semibold text-lg tracking-tight"
+					>Confirm your transaction</AlertDialog.Title
+				>
+				<AlertDialog.Description class="text-foreground-alt text-sm">
+					This action cannot be undone. This will initiate a monthly wire in the amount of $10,000
+					to Huntabyte. Do you wish to continue?
+				</AlertDialog.Description>
+			</div>
+			<div class="flex w-full items-center justify-center gap-2">
+				<AlertDialog.Cancel
+					class="h-input rounded-input inline-flex items-center justify-center w-full shadow-mini bg-muted font-medium text-[15px]"
+					>Cancel</AlertDialog.Cancel
+				>
+				<AlertDialog.Action
+					class="h-input rounded-input bg-dark inline-flex items-center justify-center w-full shadow-mini text-background font-semibold text-[15px]"
+					>Continue</AlertDialog.Action
+				>
+			</div>
+		</AlertDialog.Content>
+	</AlertDialog.Portal>
 </AlertDialog.Root>
--- src/components/demos/dialog-demo.svelte
+++ src/components/demos/dialog-demo.svelte
@@ -1,31 +1,53 @@
 <script lang="ts">
-	import { Button, buttonVariants } from "@/components/ui/button";
-	import * as Dialog from "@/components/ui/dialog";
-	import * as Input from "@/components/ui/input";
-	import * as Label from "@/components/ui/label";
+	import { Dialog } from "$lib";
+	import { Separator } from "$lib";
+	import { Button } from "$lib";
+	import { flyAndScale } from "@/utils";
+	import { fade } from "svelte/transition";
+	import { X } from "phosphor-svelte";
 </script>
 
 <Dialog.Root>
-	<Dialog.Trigger class={buttonVariants({ variant: "outline" })}>Edit Profile</Dialog.Trigger>
-	<Dialog.Content class="sm:max-w-[425px]">
-		<Dialog.Header>
-			<Dialog.Title>Edit profile</Dialog.Title>
-			<Dialog.Description>
-				Make changes to your profile here. Click save when you're done.
+	<Dialog.Trigger asChild let:builder>
+		<Button.Root
+			builders={[builder]}
+			class="rounded-input h-12 text-[15px] font-semibold px-[21px] inline-flex
+		items-center justify-center bg-dark text-background hover:button-gradient
+		shadow-mini transition-all"
+		>
+			Subscribe
+		</Button.Root>
+	</Dialog.Trigger>
+	<Dialog.Portal>
+		<Dialog.Overlay
+			transition={fade}
+			transitionConfig={{ duration: 150 }}
+			class="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm"
+		/>
+		<Dialog.Content
+			transition={flyAndScale}
+			class="fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] border bg-background p-7 rounded-card-lg md:w-full outline-none shadow-popover"
+		>
+			<Dialog.Title
+				class="font-semibold text-lg tracking-tight flex w-full justify-center items-center"
+				>Create API key</Dialog.Title
+			>
+			<Separator.Root class="-mx-7 block bg-muted h-px my-5" />
+			<Dialog.Description class="text-foreground-alt text-sm">
+				Create and manage API keys. You can create multiple keys to organize your applications.
 			</Dialog.Description>
-		</Dialog.Header>
-		<div class="grid gap-4 py-4">
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label.Root class="text-right">Name</Label.Root>
-				<Input.Root id="name" value="Hunter Johnston" class="col-span-3" />
+			<div class="flex w-full items-center justify-center gap-2">
+				<Dialog.Close
+					class="h-input rounded-input inline-flex items-center justify-center w-full shadow-mini bg-muted font-medium text-[15px]"
+					>Cancel</Dialog.Close
+				>
 			</div>
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label.Root class="text-right">Username</Label.Root>
-				<Input.Root id="username" value="@huntabyte" class="col-span-3" />
-			</div>
-		</div>
-		<Dialog.Footer>
-			<Button type="submit">Save changes</Button>
-		</Dialog.Footer>
-	</Dialog.Content>
+			<Dialog.Close
+				class="absolute right-4 top-[19px] rounded-sm transition-opacity hover:opacity-100 disabled:pointer-events-none"
+			>
+				<X class="h-4 w-4 text-foreground" />
+				<span class="sr-only">Close</span>
+			</Dialog.Close>
+		</Dialog.Content>
+	</Dialog.Portal>
 </Dialog.Root>
