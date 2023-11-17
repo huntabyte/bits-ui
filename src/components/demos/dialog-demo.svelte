eb35d76e6f81a451c3a34f0b3cbb75f8c5968ce1
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-16 21:02:57 -0500

    more dialog stuff

--- src/components/demos/dialog-demo.svelte
+++ src/components/demos/dialog-demo.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { Dialog } from "$lib";
-	import { Separator } from "$lib";
-	import { Button } from "$lib";
+	import { Dialog, Separator, Button, Label } from "$lib";
 	import { flyAndScale } from "@/utils";
 	import { fade } from "svelte/transition";
-	import { X } from "phosphor-svelte";
+	import { X, LockKeyOpen } from "phosphor-svelte";
 </script>
 
 <Dialog.Root>
@@ -26,27 +24,41 @@
 		/>
 		<Dialog.Content
 			transition={flyAndScale}
-			class="fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] border bg-background p-7 rounded-card-lg md:w-full outline-none shadow-popover"
+			class="fixed left-[50%] top-[50%] z-50 w-full max-w-[490px] translate-x-[-50%] translate-y-[-50%] border bg-background p-5 rounded-card-lg md:w-full outline-none shadow-popover"
 		>
 			<Dialog.Title
 				class="font-semibold text-lg tracking-tight flex w-full justify-center items-center"
 				>Create API key</Dialog.Title
 			>
-			<Separator.Root class="-mx-7 block bg-muted h-px my-5" />
+			<Separator.Root class="-mx-5 block bg-muted h-px mt-5 mb-6" />
 			<Dialog.Description class="text-foreground-alt text-sm">
 				Create and manage API keys. You can create multiple keys to organize your applications.
 			</Dialog.Description>
-			<div class="flex w-full items-center justify-center gap-2">
-				<Dialog.Close
-					class="h-input rounded-input inline-flex items-center justify-center w-full shadow-mini bg-muted font-medium text-[15px]"
-					>Cancel</Dialog.Close
-				>
+			<div class="flex flex-col items-start pt-7 gap-1 pb-11">
+				<Label.Root for="apiKey" class="text-sm font-medium">API Key</Label.Root>
+				<div class="relative w-full">
+					<input
+						id="apiKey"
+						class="h-input rounded-card-sm border border-dark-10 bg-background w-full focus:outline-none focus:ring-2 ring-muted px-4 inline-flex items-center placeholder:text-foreground-alt/50 text-sm"
+						placeholder="secret_api_key"
+						type="password"
+						autocomplete="off"
+					/>
+					<LockKeyOpen class="absolute right-4 top-4 text-dark/30" />
+				</div>
 			</div>
-			<Dialog.Close
-				class="absolute right-4 top-[19px] rounded-sm transition-opacity hover:opacity-100 disabled:pointer-events-none"
-			>
-				<X class="h-4 w-4 text-foreground" />
-				<span class="sr-only">Close</span>
+			<div class="flex w-full justify-end">
+				<Dialog.Close
+					class="h-input rounded-input bg-dark inline-flex items-center justify-center shadow-mini text-background font-semibold text-[15px] px-[50px] hover:button-gradient"
+				>
+					Save
+				</Dialog.Close>
+			</div>
+			<Dialog.Close class="absolute right-5 top-5">
+				<div>
+					<X class="h-5 w-5 text-foreground" />
+					<span class="sr-only">Close</span>
+				</div>
 			</Dialog.Close>
 		</Dialog.Content>
 	</Dialog.Portal>
