c98c3c6e47ac0ece47be1c94ad94dc37e06078c3
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-18 00:38:01 -0500

    switch

--- src/components/demos/switch-demo.svelte
+++ src/components/demos/switch-demo.svelte
@@ -2,16 +2,16 @@
 	import { Switch, Label } from "$lib";
 </script>
 
-<div class="flex items-center space-x-2">
+<div class="flex items-center space-x-3">
 	<Switch.Root
-		id="airplane-mode"
-		class="focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=unchecked]:bg-primary/60 peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50"
+		id="marketing"
+		class="focus-visible:ring-ring peer inline-flex h-7 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-mini-inset transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-foreground data-[state=unchecked]:bg-muted"
 	>
 		<Switch.Thumb
-			class="pointer-events-none relative inline-block h-4 w-4 transform rounded-full shadow-lg ring-0 transition-all ease-in-out data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-background data-[state=unchecked]:bg-accent"
+			class="pointer-events-none block h-6 w-6 rounded-full bg-background shadow-mini ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
 		/>
 	</Switch.Root>
-	<Label.Root for="airplane-mode" class="text-sm font-medium"
-		>Airplane Mode</Label.Root
+	<Label.Root for="marketing" class="text-sm font-medium"
+		>Do not disturb</Label.Root
 	>
 </div>
