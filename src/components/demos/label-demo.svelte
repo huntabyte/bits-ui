ca6f8a21b199b84be6b30c12811932da39058a24
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-18 00:46:55 -0500

    label demo

--- src/components/demos/label-demo.svelte
+++ src/components/demos/label-demo.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
-	import * as Checkbox from "@/components/ui/checkbox";
-	import * as Label from "@/components/ui/label";
+	import { Checkbox } from "$lib";
+	import { Label } from "$lib";
+	import { Check, Minus } from "phosphor-svelte";
 </script>
 
-<div>
-	<div class="flex items-center space-x-2">
-		<Checkbox.Root id="terms" />
-		<Label.Root for="terms">Accept terms and conditions</Label.Root>
-	</div>
+<div class="flex items-center space-x-3">
+	<Checkbox.Root
+		id="terms"
+		aria-labelledby="terms-label"
+		class="peer h-[25px] w-[25px] rounded-md border border-muted bg-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background data-[state=unchecked]:bg-background"
+		checked="indeterminate"
+	>
+		<Checkbox.Indicator
+			let:isChecked
+			let:isIndeterminate
+			class="inline-flex items-center justify-center"
+		>
+			{#if isChecked}
+				<Check class="h-[15px] w-[15px] text-background" />
+			{:else if isIndeterminate}
+				<Minus class="h-[15px] w-[15px] text-background" />
+			{/if}
+		</Checkbox.Indicator>
+	</Checkbox.Root>
+	<Label.Root
+		id="terms-label"
+		for="terms"
+		class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+	>
+		Accept terms and conditions
+	</Label.Root>
 </div>
