66b4ae53e66770a1c98985c9604284bedd4c49c4
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-19 15:12:34 -0500

    fix: switch ring

--- src/components/demos/switch-demo.svelte
+++ src/components/demos/switch-demo.svelte
@@ -5,7 +5,7 @@
 <div class="flex items-center space-x-3">
 	<Switch.Root
 		id="marketing"
-		class="focus-visible:ring-ring peer inline-flex h-7 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-mini-inset transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-foreground data-[state=unchecked]:bg-muted"
+		class="peer inline-flex h-7 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-mini-inset transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-foreground data-[state=unchecked]:bg-muted"
 	>
 		<Switch.Thumb
 			class="pointer-events-none block h-6 w-6 rounded-full bg-background shadow-mini ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
