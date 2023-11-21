44d5603601c98d5986a1230c53d66f2b2013b810
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-20 20:16:19 -0500

    radio group demo init

--- src/components/demos/radio-group-demo.svelte
+++ src/components/demos/radio-group-demo.svelte
@@ -2,34 +2,34 @@
 	import { RadioGroup, Label } from "$lib";
 </script>
 
-<RadioGroup.Root class="flex flex-col gap-3">
+<RadioGroup.Root class="flex flex-col gap-4">
 	<div
-		class="text-primary/50 [&:has([data-state=checked])]:text-primary group flex cursor-pointer select-none items-center gap-3 transition-colors"
+		class="group flex cursor-pointer select-none items-center gap-3 text-foreground/50 transition-colors [&:has([data-state=checked])]:text-foreground"
 	>
 		<RadioGroup.Item
 			id="amazing"
 			value="amazing"
-			class="border-primary/40 data-[state=checked]:group-hover:border-primary data-[state=checked]:border-primary shrink-0 rounded-full border-2 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-background/70 data-[state=checked]:border-6"
+			class="shrink-0 rounded-full border-2 border-foreground/40 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-foreground/70 data-[state=checked]:border-6 data-[state=checked]:border-foreground data-[state=checked]:group-hover:border-foreground"
 		/>
 		<Label.Root for="amazing" class="text-sm font-medium">Amazing</Label.Root>
 	</div>
 	<div
-		class="text-primary/50 [&:has([data-state=checked])]:text-primary group flex cursor-pointer select-none items-center gap-3 transition-colors"
+		class="group flex cursor-pointer select-none items-center gap-3 text-foreground/50 transition-colors [&:has([data-state=checked])]:text-foreground"
 	>
 		<RadioGroup.Item
 			id="average"
 			value="average"
-			class="border-primary/40 data-[state=checked]:group-hover:border-primary data-[state=checked]:border-primary shrink-0 rounded-full border-2 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-background/70 data-[state=checked]:border-6 "
+			class="shrink-0 rounded-full border-2 border-foreground/40 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-foreground/70 data-[state=checked]:border-6 data-[state=checked]:border-foreground data-[state=checked]:group-hover:border-foreground "
 		/>
 		<Label.Root for="average" class="text-sm font-medium">Average</Label.Root>
 	</div>
 	<div
-		class="text-primary/50 [&:has([data-state=checked])]:text-primary group flex cursor-pointer select-none items-center gap-3 transition-colors"
+		class="group flex cursor-pointer select-none items-center gap-3 text-foreground/50 transition-colors [&:has([data-state=checked])]:text-foreground"
 	>
 		<RadioGroup.Item
 			id="terrible"
 			value="terrible"
-			class="border-primary/40 data-[state=checked]:group-hover:border-primary data-[state=checked]:border-primary shrink-0 rounded-full border-2 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-background/70 data-[state=checked]:border-6"
+			class="shrink-0 rounded-full border-2 border-foreground/40 ring-offset-1 transition-all duration-200 sq-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-foreground/70 data-[state=checked]:border-6 data-[state=checked]:border-foreground data-[state=checked]:group-hover:border-foreground"
 		/>
 		<Label.Root for="terrible" class="text-sm font-medium">Terrible</Label.Root>
 	</div>
