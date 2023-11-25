f20cf5a3a2831acc9de94985ba395439b7939ef5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-25 13:50:20 -0500

    cal styles

--- content/components/calendar.md
+++ content/components/calendar.md
@@ -14,4 +14,44 @@ description: A calendar component.
 
 </ComponentPreview>
 
+## Structure
+
+```svelte
+<script lang="ts">
+	import { Calendar } from "bits-ui";
+</script>
+
+<Calendar.Root let:months let:daysOfWeek>
+	<Calendar.Header>
+		<Calendar.PrevButton />
+		<Calendar.Heading />
+		<Calendar.NextButton />
+	</Calendar.Header>
+	{#each months as month}
+		<Calendar.Grid>
+			<Calendar.GridHead>
+				<Calendar.GridRow>
+					{#each daysOfWeek as day}
+						<Calendar.HeadCell>
+							{day}
+						</Calendar.HeadCell>
+					{/each}
+				</Calendar.GridRow>
+			</Calendar.GridHead>
+			<Calendar.GridBody>
+				{#each month.weeks as weekDates}
+					<Calendar.GridRow>
+						{#each weekDates as date}
+							<Calendar.Cell {date}>
+								<Calendar.Date {date} month={month.value} />
+							</Calendar.Cell>
+						{/each}
+					</Calendar.GridRow>
+				{/each}
+			</Calendar.GridBody>
+		</Calendar.Grid>
+	{/each}
+</Calendar.Root>
+```
+
 <APISection {schemas} />
--- src/components/demos/calendar-demo.svelte
+++ src/components/demos/calendar-demo.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
 	import { Calendar } from "$lib";
-	import { cn } from "@/utils";
-	import { ChevronLeft, ChevronRight } from "lucide-svelte";
-	import { buttonVariants } from "../ui/button";
+	import { CaretRight, CaretLeft } from "phosphor-svelte";
 </script>
 
 <Calendar.Root
-	class="rounded-card border bg-background p-6 shadow-card"
+	class="mt-6 rounded-card border bg-background p-6 shadow-card"
 	let:months
 	let:daysOfWeek
 >
 	<Calendar.Header class="flex items-center justify-between">
 		<Calendar.PrevButton
-			class={cn(
-				buttonVariants({ variant: "outline" }),
-				"h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-			)}
+			class="inline-flex h-7 w-7 items-center justify-center rounded-[7px] border border-border-input bg-background shadow-btn transition-all hover:bg-muted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
 		>
-			<ChevronLeft class="h-4 w-4" />
+			<CaretLeft class="h-4 w-4" />
 		</Calendar.PrevButton>
 		<Calendar.Heading class="font-medium" />
 		<Calendar.NextButton
-			class={cn(
-				buttonVariants({ variant: "outline" }),
-				"h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-			)}
+			class="inline-flex h-7 w-7 items-center justify-center rounded-[7px] border border-border-input bg-background shadow-btn transition-all hover:bg-muted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
 		>
-			<ChevronRight class="h-4 w-4" />
+			<CaretRight class="h-4 w-4" />
 		</Calendar.NextButton>
 	</Calendar.Header>
 	<div
@@ -51,12 +43,12 @@
 							{#each weekDates as date}
 								<Calendar.Cell
 									{date}
-									class="relative h-9 w-9 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-muted first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
+									class="relative h-9 w-9 p-0 text-center text-sm focus-within:relative focus-within:z-20"
 								>
 									<Calendar.Date
 										{date}
 										month={month.value}
-										class="focus-visible:ring-ring inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-md bg-background p-0 text-sm font-normal text-foreground ring-offset-background transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-selected:bg-muted data-[outside-month]:pointer-events-none data-[outside-month]:opacity-0"
+										class="inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-[7px] bg-background p-0 text-sm font-normal text-foreground ring-offset-background transition-colors hover:bg-muted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 data-[disabled]:pointer-events-none data-[outside-month]:pointer-events-none data-[selected]:bg-foreground data-[selected]:text-background data-[disabled]:opacity-50 data-[outside-month]:opacity-20"
 									/>
 								</Calendar.Cell>
 							{/each}
