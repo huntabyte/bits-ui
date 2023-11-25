61cb4657a356e8f81c88117c6de160fd783d48c9
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-25 15:55:54 -0500

    data attr table

--- src/components/api-ref/data-attrs-table.svelte
+++ src/components/api-ref/data-attrs-table.svelte
@@ -10,8 +10,9 @@
 <Table.Root>
 	<Table.Header>
 		<Table.Row class="w-1/4">
-			<Table.Head class="w-1/2 whitespace-nowrap">Data Attribute</Table.Head>
-			<Table.Head class="w-1/2 whitespace-nowrap">Description/Value</Table.Head>
+			<Table.Head class="w-[35%] whitespace-nowrap">Data Attribute</Table.Head>
+			<Table.Head class="w-[25%] whitespace-nowrap">Value</Table.Head>
+			<Table.Head class="w-[40%] whitespace-nowrap">Description</Table.Head>
 		</Table.Row>
 	</Table.Header>
 	<Table.Body>
@@ -22,17 +23,14 @@
 						<Code class="font-semibold text-foreground">data-{attr.name}</Code>
 					</Table.Cell>
 					<Table.Cell class="align-baseline">
+						<Code class="!h-min !bg-background !px-0">{attr.value ?? "——"}</Code
+						>
+					</Table.Cell>
+					<Table.Cell class="align-baseline">
 						{#if attr.description}
 							<p class="my-2 text-sm leading-7">
 								{@html parseMarkdown(attr.description)}
 							</p>
-						{/if}
-						{#if attr.value}
-							<Code class="!h-min !bg-background !px-0"
-								>Value: {attr.value}</Code
-							>
-						{:else}
-							<Code class="!h-min !bg-background !px-0">Value: ''</Code>
 						{/if}
 					</Table.Cell>
 				</Table.Row>
