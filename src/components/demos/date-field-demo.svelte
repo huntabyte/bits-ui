673655718576d88ec856b360b07329a0c29868e9
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-27 09:50:26 -0500

    cleanup date field

--- src/components/demos/date-field-demo.svelte
+++ src/components/demos/date-field-demo.svelte
@@ -7,13 +7,13 @@
 		<DateField.Label class="block select-none">Check-in date</DateField.Label>
 		<DateField.Field
 			let:segments
-			class="flex h-input w-full max-w-[300px] grow-0 select-none items-center rounded-input border border-border-input p-3 shadow-mini"
+			class="flex h-input w-full max-w-[300px] select-none items-center rounded-input border border-border-input p-3 shadow-mini"
 		>
 			{#each segments as { part, value }}
-				<div class="inline-block select-none px-0.5">
+				<div class="inline-block select-none">
 					<DateField.Segment
 						{part}
-						class="rounded-[3px] focus-visible:bg-foreground focus-visible:font-medium focus-visible:text-background focus-visible:outline-none data-[segment=literal]:text-muted-foreground"
+						class="rounded-[3px] px-1 focus-visible:bg-foreground focus-visible:font-medium focus-visible:text-background focus-visible:outline-none data-[segment=literal]:px-0.5 data-[segment=literal]:text-muted-foreground"
 					>
 						{value}
 					</DateField.Segment>
