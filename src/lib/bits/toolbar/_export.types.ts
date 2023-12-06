9daeadf84b4e9d1313fea2ba51d146a1c827c85e
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-12-07 00:20:11 +0100

    add missing _export.types.ts file

---
+++ src/lib/bits/toolbar/_export.types.ts
@@ -0,0 +1,12 @@
+export type {
+	Props as ToolbarProps,
+	ButtonProps as ToolbarButtonProps,
+	LinkProps as ToolbarLinkProps,
+	SeparatorProps as ToolbarSeparatorProps,
+	GroupProps as ToolbarGroupProps,
+	ItemProps as ToolbarItemProps,
+	//
+	ButtonEvents as ToolbarButtonEvents,
+	LinkEvents as ToolbarLinkEvents,
+	ItemEvents as ToolbarItemEvents
+} from "./types.js";
