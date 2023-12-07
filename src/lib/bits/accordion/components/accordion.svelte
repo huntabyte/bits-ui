78eecb99cd5f4dfbe03af28d189640a3e3e90c9d
Hunter Johnston <johnstonhuntera@gmail.com>
2023-12-06 20:41:19 -0500

    fix: accordion array bind

--- src/lib/bits/accordion/components/accordion.svelte
+++ src/lib/bits/accordion/components/accordion.svelte
@@ -21,6 +21,14 @@
 		disabled,
 		defaultValue: value,
 		onValueChange: (({ next }: { next: $$Props["value"] }) => {
+			if (Array.isArray(next)) {
+				if (JSON.stringify(next) !== JSON.stringify(value)) {
+					onValueChange?.(next);
+					value = next;
+				}
+				return next;
+			}
+
 			if (value !== next) {
 				onValueChange?.(next);
 				value = next;
