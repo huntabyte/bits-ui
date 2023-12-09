8bae105b1b5adb4760517ce53b7cbe31eff81d85
Hunter Johnston <johnstonhuntera@gmail.com>
2023-12-09 12:50:03 -0500

    og image

--- src/components/metadata.svelte
+++ src/components/metadata.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { siteConfig } from "@/config";
-	import SvelteSeo from "svelte-seo";
 
 	export let title: string = siteConfig.name;
 
@@ -10,33 +9,26 @@
 		: siteConfig.name;
 </script>
 
-<SvelteSeo
-	{title}
-	description={siteConfig.description}
-	canonical={siteConfig.url}
-	keywords={siteConfig.keywords}
-	openGraph={{
-		title,
-		description: siteConfig.description,
-		images: [
-			{
-				url: siteConfig.ogImage,
-				width: 1200,
-				height: 630,
-				alt: siteConfig.name
-			}
-		],
-		site_name: siteConfig.name,
-		url: siteConfig.url,
-		type: "website"
-	}}
-	twitter={{
-		card: "summary_large_image",
-		site: "@huntabyte",
-		title,
-		description: siteConfig.description,
-		image: siteConfig.ogImage,
-		creator: "@huntabyte",
-		imageAlt: siteConfig.description
-	}}
-/>
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={siteConfig.description} />
+	<meta name="keywords" content={siteConfig.keywords} />
+	<meta name="author" content="huntabyte" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content={siteConfig.url} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={siteConfig.description} />
+	<meta name="twitter:image" content="https://www.bits-ui.com/og.png" />
+	<meta name="twitter:image:alt" content={siteConfig.name} />
+	<meta name="twitter:creator" content="huntabyte" />
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={siteConfig.url + $page.url.pathname} />
+	<meta property="og:image" content="https://www.bits-ui.com/og.png" />
+	<meta property="og:image:alt" content={siteConfig.name} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:description" content={siteConfig.description} />
+	<meta property="og:site_name" content={siteConfig.name} />
+	<meta property="og:locale" content="EN_US" />
+</svelte:head>
