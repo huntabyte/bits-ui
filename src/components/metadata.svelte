c5b7a80d3bea288c547e8639984fa4397a906fe6
Hunter Johnston <johnstonhuntera@gmail.com>
2023-12-09 11:16:22 -0500

    add metadata

--- src/components/metadata.svelte
+++ src/components/metadata.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { siteConfig } from "@/config";
+	import SvelteSeo from "svelte-seo";
 
 	export let title: string = siteConfig.name;
 
@@ -9,9 +10,33 @@
 		: siteConfig.name;
 </script>
 
-<svelte:head>
-	<title>{title}</title>
-	<meta name="description" content={siteConfig.description} />
-	<meta name="keywords" content={siteConfig.keywords} />
-	<meta name="author" content="huntabyte" />
-</svelte:head>
+<SvelteSeo
+	{title}
+	description={siteConfig.description}
+	canonical={siteConfig.url}
+	keywords={siteConfig.keywords}
+	openGraph={{
+		title,
+		description: siteConfig.description,
+		images: [
+			{
+				url: siteConfig.ogImage,
+				width: 1200,
+				height: 630,
+				alt: siteConfig.name
+			}
+		],
+		site_name: siteConfig.name,
+		url: siteConfig.url,
+		type: "website"
+	}}
+	twitter={{
+		card: "summary_large_image",
+		site: "@huntabyte",
+		title,
+		description: siteConfig.description,
+		image: siteConfig.ogImage,
+		creator: "@huntabyte",
+		imageAlt: siteConfig.description
+	}}
+/>
