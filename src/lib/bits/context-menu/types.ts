b16d8b3d61427756c9e9c339436738a95a7ca46d
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-25 12:45:34 +0000

    feat: expose el prop for context-menu components

--- src/lib/bits/context-menu/types.ts
+++ src/lib/bits/context-menu/types.ts
@@ -23,14 +23,14 @@ import type {
 	ContextTriggerEvents as TriggerEvents,
 	SubContentEvents
 } from "$lib/bits/menu/index.js";
-import type { HTMLDivAttributes, Transition } from "$lib/internal";
+import type { DOMEl, HTMLDivAttributes, Transition } from "$lib/internal";
 import type * as I from "./_types.js";
 
 type ContentProps<
 	T extends Transition = Transition,
 	In extends Transition = Transition,
 	Out extends Transition = Transition
-> = I.ContentProps<T, In, Out> & HTMLDivAttributes;
+> = I.ContentProps<T, In, Out> & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
 export type {
 	Props,
