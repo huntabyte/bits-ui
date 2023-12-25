0914a1fde68986eac260cbe6c42ebf705c190f8c
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-25 14:06:35 +0000

    feat: expose el prop for dialog components

--- src/lib/bits/dialog/types.ts
+++ src/lib/bits/dialog/types.ts
@@ -1,29 +1,34 @@
-import type { HTMLDivAttributes, HTMLHeadingAttributes, Transition } from "$lib/internal/index.js";
+import type {
+	DOMEl,
+	HTMLDivAttributes,
+	HTMLHeadingAttributes,
+	Transition
+} from "$lib/internal/index.js";
 import type { HTMLButtonAttributes } from "svelte/elements";
 import type { CustomEventHandler } from "$lib/index.js";
 import type * as I from "./_types.js";
 
 type Props = I.Props;
 
-type TriggerProps = I.TriggerProps & HTMLButtonAttributes;
+type TriggerProps = I.TriggerProps & HTMLButtonAttributes & DOMEl<HTMLButtonElement>;
 type CloseProps = TriggerProps;
 
 type ContentProps<
 	T extends Transition = Transition,
 	In extends Transition = Transition,
 	Out extends Transition = Transition
-> = I.ContentProps<T, In, Out> & HTMLDivAttributes;
+> = I.ContentProps<T, In, Out> & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
-type DescriptionProps = I.DescriptionProps & HTMLDivAttributes;
+type DescriptionProps = I.DescriptionProps & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
 type OverlayProps<
 	T extends Transition = Transition,
 	In extends Transition = Transition,
 	Out extends Transition = Transition
-> = I.OverlayProps<T, In, Out> & HTMLDivAttributes;
+> = I.OverlayProps<T, In, Out> & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
-type PortalProps = I.PortalProps & HTMLDivAttributes;
-type TitleProps = I.TitleProps & HTMLHeadingAttributes;
+type PortalProps = I.PortalProps & HTMLDivAttributes & DOMEl<HTMLDivElement>;
+type TitleProps = I.TitleProps & HTMLHeadingAttributes & DOMEl<HTMLHeadingElement>;
 
 type TriggerEvents<T extends Element = HTMLButtonElement> = {
 	click: CustomEventHandler<MouseEvent, T>;
