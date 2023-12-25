2795033ed9b9f9822be669c51a4d51f938312527
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-25 12:01:47 +0000

    feat: expose el prop for checkbox components

--- src/lib/bits/checkbox/types.ts
+++ src/lib/bits/checkbox/types.ts
@@ -1,13 +1,13 @@
-import type { HTMLDivAttributes } from "$lib/internal/index.js";
+import type { DOMEl, HTMLDivAttributes } from "$lib/internal/index.js";
 import type { CustomEventHandler } from "$lib/index.js";
 import type { HTMLButtonAttributes, HTMLInputAttributes } from "svelte/elements";
 import type * as I from "./_types.js";
 
-type Props = I.Props & HTMLButtonAttributes;
+type Props = I.Props & HTMLButtonAttributes & DOMEl<HTMLButtonElement>;
 
-type IndicatorProps = I.IndicatorProps & HTMLDivAttributes;
+type IndicatorProps = I.IndicatorProps & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
-type InputProps = Omit<HTMLInputAttributes, "value">;
+type InputProps = Omit<HTMLInputAttributes, "value"> & DOMEl<HTMLInputElement>;
 
 type Events = {
 	click: CustomEventHandler<MouseEvent, HTMLButtonElement>;
