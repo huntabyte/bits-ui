7b3e18beaddcfbf5a982334a3c6430382bf69fcd
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-25 15:22:32 +0000

    feat: expose el prop for toggle-group components

--- src/lib/bits/toggle-group/types.ts
+++ src/lib/bits/toggle-group/types.ts
@@ -1,11 +1,11 @@
-import type { HTMLDivAttributes } from "$lib/internal/index.js";
+import type { DOMEl, HTMLDivAttributes } from "$lib/internal/index.js";
 import type { HTMLButtonAttributes } from "svelte/elements";
 import type { CustomEventHandler } from "$lib/index.js";
 import type * as I from "./_types.js";
 
-type Props<T extends "single" | "multiple"> = I.Props<T> & HTMLDivAttributes;
+type Props<T extends "single" | "multiple"> = I.Props<T> & HTMLDivAttributes & DOMEl;
 
-type ItemProps = I.ItemProps & HTMLButtonAttributes;
+type ItemProps = I.ItemProps & HTMLButtonAttributes & DOMEl<HTMLButtonElement>;
 
 type ItemEvents<T extends Element = HTMLButtonElement> = {
 	click: CustomEventHandler<MouseEvent, T>;
