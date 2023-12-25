ab78c88bff1239e97ecfc743bfdd144108f4d2e4
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-25 13:16:36 +0000

    feat: expose el prop for date-range-field components

--- src/lib/bits/date-range-field/types.ts
+++ src/lib/bits/date-range-field/types.ts
@@ -1,10 +1,10 @@
-import type { HTMLDivAttributes, HTMLSpanAttributes } from "$lib/internal/index.js";
+import type { DOMEl, HTMLDivAttributes, HTMLSpanAttributes } from "$lib/internal/index.js";
 import type * as I from "./_types.js";
 import type { SegmentEvents } from "../date-field/types.js";
 
 type Props = I.Props;
-type LabelProps = I.LabelProps & HTMLSpanAttributes;
-type SegmentProps = I.SegmentProps & HTMLDivAttributes;
-type InputProps = I.InputProps & HTMLDivAttributes;
+type LabelProps = I.LabelProps & HTMLSpanAttributes & DOMEl<HTMLSpanElement>;
+type SegmentProps = I.SegmentProps & HTMLDivAttributes & DOMEl<HTMLDivElement>;
+type InputProps = I.InputProps & HTMLDivAttributes & DOMEl<HTMLDivElement>;
 
 export type { Props, LabelProps, InputProps, SegmentProps, SegmentEvents };
