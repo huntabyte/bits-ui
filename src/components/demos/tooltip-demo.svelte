cf3df4c88ecca1553ea7d689850a449dd77ee98c
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-07 22:12:26 -0500

    tooltip example

--- src/components/demos/tooltip-demo.svelte
+++ src/components/demos/tooltip-demo.svelte
@@ -4,7 +4,7 @@
 	import { MagicWand } from "phosphor-svelte";
 </script>
 
-<Tooltip.Root openDelay={200} open={true}>
+<Tooltip.Root openDelay={0}>
 	<Tooltip.Trigger
 		class="inline-flex size-10 items-center justify-center rounded-full
 	border border-border-input bg-background shadow-btn ring-dark ring-offset-background hover:bg-muted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-dark focus-visible:ring-offset-2 focus-visible:ring-offset-background"
@@ -14,10 +14,15 @@
 	<Tooltip.Content
 		transition={flyAndScale}
 		transitionConfig={{ y: 8, duration: 150 }}
-		class="flex items-center justify-center rounded-input border border-dark-10 bg-background p-3 text-sm font-medium shadow-popover outline-none"
 		sideOffset={8}
 	>
-		<Tooltip.Arrow class="border-l border-t border-dark-10" />
-		<div>Make some magic!</div>
+		<div class="bg-background">
+			<Tooltip.Arrow class="rounded-[2px] border-l border-t border-dark-10" />
+		</div>
+		<div
+			class="flex items-center justify-center rounded-input border border-dark-10 bg-background p-3 text-sm font-medium shadow-popover outline-none"
+		>
+			Make some magic!
+		</div>
 	</Tooltip.Content>
 </Tooltip.Root>
