b587295ebbaf2087207164d5bb57dbf3f6880116
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-07 23:06:34 -0500

    improve range calendar tyles

--- src/components/demos/date-range-field-demo.svelte
+++ src/components/demos/date-range-field-demo.svelte
@@ -9,35 +9,47 @@
 </script>
 
 <DateRangeField.Root bind:value>
-	<div class="flex min-w-[200px] flex-col gap-1">
-		<DateRangeField.Label class="block select-none"
+	<div class="flex w-full max-w-[300px] flex-col gap-1.5">
+		<DateRangeField.Label class="block select-none text-sm font-medium"
 			>Hotel dates</DateRangeField.Label
 		>
 		<DateRangeField.Input
 			let:segments
-			class="flex h-input w-full max-w-[300px] select-none items-center rounded-input border border-border-input bg-background p-3 text-sm shadow-mini"
+			class="flex h-input w-full max-w-[300px] select-none items-center rounded-input border border-border-input bg-background p-3 text-sm tracking-[0.01em] text-muted-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
 		>
 			{#each segments.start as { part, value }}
 				<div class="inline-block select-none">
-					<DateRangeField.Segment
-						type="start"
-						{part}
-						class="rounded-[3px] px-1 data-[segment=literal]:px-px data-[segment=literal]:text-muted-foreground"
-					>
-						{value}
-					</DateRangeField.Segment>
+					{#if part === "literal"}
+						<DateRangeField.Segment type="start" {part} class="p-1">
+							{value}
+						</DateRangeField.Segment>
+					{:else}
+						<DateRangeField.Segment
+							type="start"
+							{part}
+							class="rounded-5px px-2 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0"
+						>
+							{value}
+						</DateRangeField.Segment>
+					{/if}
 				</div>
 			{/each}
 			<div aria-hidden class="px-1">-</div>
 			{#each segments.end as { part, value }}
 				<div class="inline-block select-none">
-					<DateRangeField.Segment
-						type="end"
-						{part}
-						class="rounded-[3px] px-1 data-[segment=literal]:px-px data-[segment=literal]:text-muted-foreground"
-					>
-						{value}
-					</DateRangeField.Segment>
+					{#if part === "literal"}
+						<DateRangeField.Segment type="end" {part} class="p-1">
+							{value}
+						</DateRangeField.Segment>
+					{:else}
+						<DateRangeField.Segment
+							type="end"
+							{part}
+							class="rounded-5px px-2 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0"
+						>
+							{value}
+						</DateRangeField.Segment>
+					{/if}
 				</div>
 			{/each}
 		</DateRangeField.Input>
