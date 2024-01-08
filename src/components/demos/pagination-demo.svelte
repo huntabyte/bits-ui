567619c5c68ca1dd64b52700cdf198d88984770c
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-07 20:09:30 -0500

    update pagination demo

--- src/components/demos/pagination-demo.svelte
+++ src/components/demos/pagination-demo.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-	import { Pagination } from "$lib";
-	import { CaretLeft, CaretRight, DotsThree } from "phosphor-svelte";
+	import { Pagination } from "$lib/index.js";
+	import { CaretLeft, CaretRight } from "phosphor-svelte";
 </script>
 
 <Pagination.Root count={100} perPage={10} let:pages let:range>
-	<p class="text-sm text-muted-foreground">
-		Showing {range.start} - {range.end}
-	</p>
-	<div class="mt-4 flex items-center justify-center gap-1.5">
+	<div class="mb-[55px] flex items-center">
 		<Pagination.PrevButton
-			class="inline-flex items-center justify-center rounded-[9px] bg-transparent transition-all sq-10 hover:bg-foreground/5 active:scale-98 active:bg-dark-10 disabled:cursor-not-allowed disabled:opacity-50 hover:disabled:bg-transparent"
+			class="mr-[25px] inline-flex items-center justify-center rounded-[9px] bg-transparent sq-10 hover:bg-muted active:scale-98 disabled:cursor-not-allowed disabled:text-muted-foreground hover:disabled:bg-transparent"
 		>
 			<CaretLeft class="sq-6" />
 		</Pagination.PrevButton>
-		{#each pages as page (page.key)}
-			{#if page.type === "ellipsis"}
-				<div class="self-end">
-					<DotsThree class="sq-5" weight="bold" />
-				</div>
-			{:else}
-				<Pagination.Page
-					{page}
-					class="inline-flex items-center justify-center rounded-[9px] border border-border bg-background shadow-mini sq-10 hover:bg-muted active:scale-98 active:bg-dark-10 data-[selected]:bg-foreground data-[selected]:text-background"
-				>
-					{page.value}
-				</Pagination.Page>
-			{/if}
-		{/each}
+		<div class="flex items-center gap-2.5">
+			{#each pages as page (page.key)}
+				{#if page.type === "ellipsis"}
+					<div class="text-[15px] font-medium text-foreground-alt">...</div>
+				{:else}
+					<Pagination.Page
+						{page}
+						class="inline-flex items-center justify-center rounded-[9px] bg-transparent text-[15px] font-medium sq-10 hover:bg-muted active:scale-98 disabled:cursor-not-allowed disabled:opacity-50 hover:disabled:bg-transparent data-[selected]:bg-foreground data-[selected]:text-background"
+					>
+						{page.value}
+					</Pagination.Page>
+				{/if}
+			{/each}
+		</div>
 		<Pagination.NextButton
-			class="inline-flex items-center justify-center rounded-[9px] bg-transparent transition-all sq-10 hover:bg-foreground/5 active:scale-98 active:bg-dark-10 disabled:cursor-not-allowed hover:disabled:bg-transparent"
+			class="ml-[29px] inline-flex items-center justify-center rounded-[9px] bg-transparent sq-10 hover:bg-muted active:scale-98 disabled:cursor-not-allowed disabled:text-muted-foreground hover:disabled:bg-transparent"
 		>
 			<CaretRight class="sq-6" />
 		</Pagination.NextButton>
 	</div>
+	<p class="text-center text-[13px] text-muted-foreground">
+		Showing {range.start} - {range.end}
+	</p>
 </Pagination.Root>
