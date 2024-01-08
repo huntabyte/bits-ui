a75c567c88a8bfff25922e886a460eef62a72145
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-07 23:22:34 -0500

    improve avatar demo

--- src/components/demos/avatar-demo.svelte
+++ src/components/demos/avatar-demo.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { Avatar } from "$lib";
+	export let loadingStatus: Avatar.Props["loadingStatus"] = undefined;
 </script>
 
 <Avatar.Root
-	class="flex h-12 w-12 items-center justify-center rounded-full bg-muted text-[17px] font-medium uppercase text-muted-foreground"
+	bind:loadingStatus
+	class="h-12 w-12 rounded-full border {loadingStatus === 'loaded'
+		? 'border-foreground'
+		: 'border-transparent'} bg-muted text-[17px] font-medium uppercase text-muted-foreground"
 >
-	<Avatar.Image src="/avatar-1.png" alt="@huntabyte" />
-	<Avatar.Fallback>HB</Avatar.Fallback>
+	<div
+		class="flex h-full w-full items-center justify-center rounded-full border border-background"
+	>
+		<Avatar.Image src="/avatar-1.png" alt="@huntabyte" />
+		<Avatar.Fallback class="border border-muted">HB</Avatar.Fallback>
+	</div>
 </Avatar.Root>
