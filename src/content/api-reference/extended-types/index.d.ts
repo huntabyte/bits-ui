699e20eea08d95c530fe71821edbe196b58ca70f
Robert Sweeney <robert@facet.net>
2024-01-13 17:30:42 -0700

    Remove generated .d.ts file in src.

--- src/content/api-reference/extended-types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { PropType } from "@/types/index.js";
-export declare const monthsPropType: PropType;
-export declare const focusProp: PropType;
-export declare const pageItemProp: PropType;
