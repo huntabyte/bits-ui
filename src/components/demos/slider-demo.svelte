d3a00925f4e57fa23f19ecf20eba50067bc92e51
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-04 13:58:48 -0500

    fix: hover style

--- src/components/demos/slider-demo.svelte
+++ src/components/demos/slider-demo.svelte
@@ -18,7 +18,7 @@
 		{#each thumbs as thumb}
 			<Slider.Thumb
 				{thumb}
-				class="block cursor-pointer rounded-full border border-border-input bg-background shadow transition-colors sq-[27px] hover:hover:border-dark-40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 active:scale-98 disabled:pointer-events-none disabled:opacity-50"
+				class="block cursor-pointer rounded-full border border-border-input bg-background shadow transition-colors sq-[27px] hover:border-dark-40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 active:scale-98 disabled:pointer-events-none disabled:opacity-50"
 			/>
 		{/each}
 	</Slider.Root>
