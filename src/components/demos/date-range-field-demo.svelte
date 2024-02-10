8200e4c317874f27aeaab7f186419dba5d32161c
Pavel Stianko <stianko@volny.cz>
2024-02-10 21:18:43 +0100

    Different placeholder-value colors for date fields

--- src/components/demos/date-range-field-demo.svelte
+++ src/components/demos/date-range-field-demo.svelte
@@ -15,37 +15,37 @@
 		>
 		<DateRangeField.Input
 			let:segments
-			class="flex h-input w-full max-w-[280px] select-none items-center rounded-input border border-border-input bg-background px-2 py-3 text-sm tracking-[0.01em] text-muted-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+			class="flex h-input w-full max-w-[280px] select-none items-center rounded-input border border-border-input bg-background px-2 py-3 text-sm tracking-[0.01em] text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
 		>
 			{#each segments.start as { part, value }}
 				<div class="inline-block select-none">
 					{#if part === "literal"}
-						<DateRangeField.Segment type="start" {part} class="p-1">
+						<DateRangeField.Segment type="start" {part} class="p-1 text-muted-foreground">
 							{value}
 						</DateRangeField.Segment>
 					{:else}
 						<DateRangeField.Segment
 							type="start"
 							{part}
-							class="rounded-5px px-1 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0"
+							class="rounded-5px px-1 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0 aria-[valuetext=Empty]:text-muted-foreground"
 						>
 							{value}
 						</DateRangeField.Segment>
 					{/if}
 				</div>
 			{/each}
-			<div aria-hidden class="px-1">–⁠⁠⁠⁠⁠</div>
+			<div aria-hidden class="px-1 text-muted-foreground">–⁠⁠⁠⁠⁠</div>
 			{#each segments.end as { part, value }}
 				<div class="inline-block select-none">
 					{#if part === "literal"}
-						<DateRangeField.Segment type="end" {part} class="p-1">
+						<DateRangeField.Segment type="end" {part} class="p-1 text-muted-foreground ">
 							{value}
 						</DateRangeField.Segment>
 					{:else}
 						<DateRangeField.Segment
 							type="end"
 							{part}
-							class="rounded-5px px-1 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0"
+							class="rounded-5px px-1 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0 aria-[valuetext=Empty]:text-muted-foreground"
 						>
 							{value}
 						</DateRangeField.Segment>
