230949a364da78df867fbdc7431c9629129e157e
Pavel Stianko <stianko@volny.cz>
2024-02-17 15:44:43 +0100

    format

--- src/components/demos/pin-input-demo.svelte
+++ src/components/demos/pin-input-demo.svelte
@@ -10,20 +10,23 @@
 	$: pinInputType = unlocked ? "text" : "password";
 </script>
 
-<PinInput.Root
-	bind:value
-	class="flex items-center gap-2"
-	type={pinInputType}
-	placeholder=0
->
-<PinInput.Input class="font-alt text-[17px] placeholder-shown:border-border-input w-10 text-center flex h-input select-none rounded-input border border-foreground bg-background px-2 py-3 tracking-[0.01em] text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover" />
-	<PinInput.Input class="font-alt text-[17px] placeholder-shown:border-border-input w-10 text-center flex h-input select-none rounded-input border border-foreground bg-background px-2 py-3 tracking-[0.01em] text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover" />
-	<PinInput.Input class="font-alt text-[17px] placeholder-shown:border-border-input w-10 text-center flex h-input select-none rounded-input border border-foreground bg-background px-2 py-3 tracking-[0.01em] text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover" />
-	<PinInput.Input class="font-alt text-[17px] placeholder-shown:border-border-input w-10 text-center flex h-input select-none rounded-input border border-foreground bg-background px-2 py-3 tracking-[0.01em] text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover" />
+<PinInput.Root bind:value class="flex items-center gap-2" type={pinInputType} placeholder="0">
+	<PinInput.Input
+		class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+	/>
+	<PinInput.Input
+		class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+	/>
+	<PinInput.Input
+		class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+	/>
+	<PinInput.Input
+		class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+	/>
 	<PinInput.HiddenInput />
 	<Toggle.Root
 		aria-label="toggle code visibility"
-		class="inline-flex size-10 items-center justify-center rounded-[9px] transition-all text-foreground/40 hover:bg-muted active:scale-98 active:bg-dark-10 active:data-[state=on]:bg-dark-10"
+		class="inline-flex size-10 items-center justify-center rounded-[9px] text-foreground/40 transition-all hover:bg-muted active:scale-98 active:bg-dark-10 active:data-[state=on]:bg-dark-10"
 		bind:pressed={unlocked}
 	>
 		{#if unlocked}
