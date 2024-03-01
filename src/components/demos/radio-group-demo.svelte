26f326d9ab8d8c09c06e90f43177e5556d6d8d70
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-02-29 22:52:52 -0600

    chore: update Melt UI (#360)

--- src/components/demos/radio-group-demo.svelte
+++ src/components/demos/radio-group-demo.svelte
@@ -3,34 +3,28 @@
 </script>
 
 <RadioGroup.Root class="flex flex-col gap-4 text-sm font-medium">
-	<div
-		class="group flex cursor-pointer select-none items-center gap-3 text-foreground transition-all"
-	>
+	<div class="group flex select-none items-center text-foreground transition-all">
 		<RadioGroup.Item
 			id="amazing"
 			value="amazing"
-			class="size-5 shrink-0 rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
+			class="size-5 shrink-0 cursor-default rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
 		/>
-		<Label.Root for="amazing">Amazing</Label.Root>
+		<Label.Root for="amazing" class="pl-3">Amazing</Label.Root>
 	</div>
-	<div
-		class="group flex cursor-pointer select-none items-center gap-3 text-foreground transition-all"
-	>
+	<div class="group flex select-none items-center text-foreground transition-all">
 		<RadioGroup.Item
 			id="average"
 			value="average"
-			class="size-5 shrink-0 rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
+			class="size-5 shrink-0 cursor-default rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
 		/>
-		<Label.Root for="average">Average</Label.Root>
+		<Label.Root for="average" class="pl-3">Average</Label.Root>
 	</div>
-	<div
-		class="group flex cursor-pointer select-none items-center gap-3 text-foreground transition-all"
-	>
+	<div class="group flex select-none items-center text-foreground transition-all">
 		<RadioGroup.Item
 			id="terrible"
 			value="terrible"
-			class="size-5 shrink-0 rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
+			class="size-5 shrink-0 cursor-default rounded-full border border-border-input bg-background transition-all duration-100 ease-in-out hover:border-dark-40 data-[state=checked]:border-6 data-[state=checked]:border-foreground"
 		/>
-		<Label.Root for="terrible">Terrible</Label.Root>
+		<Label.Root for="terrible" class="pl-3">Terrible</Label.Root>
 	</div>
 </RadioGroup.Root>
