cbb35835b2bd671a20e403685659f06869a2d3e4
Teodor Källman <94024065+RedPhoenixQ@users.noreply.github.com>
2024-03-12 23:29:31 +0100

    Wrap CustomEventHandler with CustomEvent
    Svelte forwarded events are wrapped in a `CustomEvent` (clearly seen when logging the received event from svelte). This fixes incorrect `.detail` types

--- src/lib/helpers/event-handlers.ts
+++ src/lib/helpers/event-handlers.ts
@@ -1,4 +1,4 @@
-export type CustomEventHandler<T extends Event = Event, M extends Element = Element> = T & {
-	currentTarget: EventTarget & M;
-	originalEvent: T;
-};
+export type CustomEventHandler<T extends Event = Event, M extends Element = Element> = CustomEvent<{
+    currentTarget: EventTarget & M;
+    originalEvent: T;
+}>;
