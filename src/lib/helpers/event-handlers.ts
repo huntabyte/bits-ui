1def98f34232d177c3c8ec509b6a214468579745
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-13 12:35:37 +0100

    Correct formatting

--- src/lib/helpers/event-handlers.ts
+++ src/lib/helpers/event-handlers.ts
@@ -1,4 +1,4 @@
 export type CustomEventHandler<T extends Event = Event, M extends Element = Element> = CustomEvent<{
-    currentTarget: EventTarget & M;
-    originalEvent: T;
+	currentTarget: EventTarget & M;
+	originalEvent: T;
 }>;
