dd5be1f454b19eff97d5bb619818a2834babd1d0
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-04-01 15:42:14 -0400

    fix postcss plugin setup

--- sites/docs/postcss.config.cjs
+++ sites/docs/postcss.config.cjs
@@ -1,7 +1,16 @@
-module.exports = {
-	plugins: {
-		"tailwindcss/nesting": {},
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+const path = require("node:path");
+const tailwindcss = require("tailwindcss");
+const tailwindNesting = require("tailwindcss/nesting");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+	plugins: [
+		tailwindNesting,
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		tailwindcss(path.resolve(__dirname, "./tailwind.config.js")),
+		//But others, like autoprefixer, need to run after,
+		autoprefixer,
+	],
 };
+
+module.exports = config;
