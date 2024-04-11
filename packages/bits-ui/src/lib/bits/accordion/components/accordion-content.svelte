6cc1b422b689f2fa8e729134b3027073d42123f0
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-11 14:51:19 -0400

    urg eslint

--- packages/bits-ui/src/lib/bits/accordion/components/accordion-content.svelte
+++ packages/bits-ui/src/lib/bits/accordion/components/accordion-content.svelte
@@ -1,4 +1,5 @@
 <script lang="ts" context="module">
+	import type { Transition } from "$lib/internal/index.js";
 	type T = Transition;
 	type In = Transition;
 	type Out = Transition;
@@ -7,7 +8,6 @@
 <script lang="ts" generics="T extends Transition, In extends Transition, Out extends Transition">
 	import { getAccordionContentState } from "../state.svelte.js";
 	import type { AccordionContentProps } from "../types.js";
-	import type { Transition } from "$lib/internal/index.js";
 	import WithTransition from "$lib/bits/utilities/with-transition.svelte";
 
 	let { child, asChild, ...props }: AccordionContentProps<T, In, Out> = $props();
