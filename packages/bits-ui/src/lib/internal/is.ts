cda8288d6da0d9a6d3c27a0ede7daa6271a5ebf9
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-04-17 19:34:52 +0300

    add utils

--- packages/bits-ui/src/lib/internal/is.ts
+++ packages/bits-ui/src/lib/internal/is.ts
@@ -3,3 +3,11 @@ export const isBrowser = typeof document !== "undefined";
 export function isFunction(value: unknown): value is (...args: unknown[]) => unknown {
 	return typeof value === "function";
 }
+
+export function isHTMLElement(element: unknown): element is HTMLElement {
+	return element instanceof HTMLElement;
+}
+
+export function isElement(element: unknown): element is Element {
+	return element instanceof Element;
+}
