4a56b964dd26639c105e340e0109474296858439
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-04-17 19:35:43 +0300

    add dismissable layer

---
+++ packages/bits-ui/src/lib/dismissable-layer/dismissable-layer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { DismissableLayerProps } from "./types.js";
+	import { dismissableLayerState } from "./dismissable-layer.svelte.js";
+	import { readonlyBox } from "$lib/internal/box.svelte.js";
+	import { noop } from "$lib/index.js";
+
+	let {
+		behaviorType = "close",
+		onInteractOutside = noop,
+		onInteractOutsideStart = noop,
+		id,
+		children,
+	}: DismissableLayerProps = $props();
+
+	dismissableLayerState({
+		id: readonlyBox(() => id),
+		behaviorType: readonlyBox(() => behaviorType),
+		onInteractOutside: readonlyBox(() => onInteractOutside),
+		onInteractOutsideStart: readonlyBox(() => onInteractOutsideStart),
+	});
+</script>
+
+{@render children?.()}
