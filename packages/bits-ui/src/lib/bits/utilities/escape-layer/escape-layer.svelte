71456ceb2a5974dfbbd8608715633005fa0cc8b5
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-04-18 17:34:41 +0200

    next: escape layer (#481)

---
+++ packages/bits-ui/src/lib/bits/utilities/escape-layer/escape-layer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { EscapeLayerProps } from "./types.js";
+	import { escapeLayerState } from "./escape-layer.svelte.js";
+	import { readonlyBox } from "$lib/internal/box.svelte.js";
+	import { noop } from "$lib/index.js";
+
+	let { behaviorType = "close", onEscape = noop, children }: EscapeLayerProps = $props();
+
+	escapeLayerState({
+		behaviorType: readonlyBox(() => behaviorType),
+		onEscape: readonlyBox(() => onEscape),
+	});
+</script>
+
+{@render children?.()}
