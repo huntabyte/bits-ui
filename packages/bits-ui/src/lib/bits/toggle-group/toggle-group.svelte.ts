1ababfeec9baa3a96110eb42c8776f9d1d982ee6
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-22 11:24:22 -0400

    toggle group work

--- packages/bits-ui/src/lib/bits/toggle-group/toggle-group.svelte.ts
+++ packages/bits-ui/src/lib/bits/toggle-group/toggle-group.svelte.ts
@@ -1,7 +1,17 @@
-import { getAttrAndSelector, getDataOrientation } from "$lib/internal/attrs.js";
+import { getContext, setContext } from "svelte";
+import {
+	getAriaChecked,
+	getAriaPressed,
+	getAttrAndSelector,
+	getDataDisabled,
+	getDataOrientation,
+} from "$lib/internal/attrs.js";
 import type { Box, BoxedValues, ReadonlyBoxedValues } from "$lib/internal/box.svelte.js";
+import { getDirectionalKeys, kbd } from "$lib/internal/kbd.js";
+import { getElemDirection } from "$lib/internal/locale.js";
 import { useNodeById } from "$lib/internal/use-node-by-id.svelte.js";
 import type { Orientation } from "$lib/shared/index.js";
+import { verifyContextDeps } from "$lib/internal/context.js";
 
 const [ROOT_ATTR] = getAttrAndSelector("toggle-group-root");
 const [ITEM_ATTR, ITEM_SELECTOR] = getAttrAndSelector("toggle-group-item");
@@ -44,6 +54,7 @@ class ToggleGroupBaseState {
 		[ROOT_ATTR]: "",
 		role: "group",
 		"data-orientation": getDataOrientation(this.orientation.value),
+		"data-disabled": getDataDisabled(this.disabled.value),
 	} as const);
 }
 
@@ -58,6 +69,7 @@ type ToggleGroupSingleStateProps = ToggleGroupBaseStateProps &
 
 class ToggleGroupSingleState extends ToggleGroupBaseState {
 	value = undefined as unknown as ToggleGroupSingleStateProps["value"];
+	isMulti = false;
 
 	constructor(props: ToggleGroupSingleStateProps) {
 		super(props);
@@ -70,6 +82,10 @@ class ToggleGroupSingleState extends ToggleGroupBaseState {
 
 	toggleItem(item: string) {
 		this.value.value = this.includesItem(item) ? "" : item;
+	}
+
+	anyPressed() {
+		return this.value.value !== "";
 	}
 }
 
@@ -84,6 +100,7 @@ type ToggleGroupMultipleStateProps = ToggleGroupBaseStateProps &
 
 class ToggleGroupMultipleState extends ToggleGroupBaseState {
 	value = undefined as unknown as ToggleGroupMultipleStateProps["value"];
+	isMulti = true;
 
 	constructor(props: ToggleGroupMultipleStateProps) {
 		super(props);
@@ -100,6 +117,10 @@ class ToggleGroupMultipleState extends ToggleGroupBaseState {
 		} else {
 			this.value.value.push(item);
 		}
+	}
+
+	anyPressed() {
+		return this.value.value.length > 0;
 	}
 }
 
@@ -118,17 +139,157 @@ type ToggleGroupItemStateProps = ReadonlyBoxedValues<{
 };
 
 class ToggleGroupItemState {
-	id = undefined as unknown as ToggleGroupItemStateProps["id"];
-	root = undefined as unknown as ToggleGroupItemStateProps["rootState"];
-	value = undefined as unknown as ToggleGroupItemStateProps["value"];
-	node: Box<HTMLElement | null>;
-	disabled = undefined as unknown as ToggleGroupItemStateProps["disabled"];
+	#id = undefined as unknown as ToggleGroupItemStateProps["id"];
+	#root = undefined as unknown as ToggleGroupItemStateProps["rootState"];
+	#value = undefined as unknown as ToggleGroupItemStateProps["value"];
+	#node: Box<HTMLElement | null>;
+	#disabled = undefined as unknown as ToggleGroupItemStateProps["disabled"];
+	#isDisabled = $derived(this.#disabled.value || this.#root.disabled.value);
 
 	constructor(props: ToggleGroupItemStateProps) {
-		this.value = props.value;
-		this.disabled = props.disabled;
-		this.root = props.rootState;
-		this.id = props.id;
-		this.node = useNodeById(this.id);
+		this.#value = props.value;
+		this.#disabled = props.disabled;
+		this.#root = props.rootState;
+		this.#id = props.id;
+		this.#node = useNodeById(this.#id);
+
+		$effect(() => {
+			const node = this.#node.value;
+			if (!node) return;
+			const items = this.#root.getItemNodes();
+			const anyPressed = this.#root.anyPressed();
+			if (!anyPressed && items[0] === node) {
+				node.tabIndex = 0;
+			}
+		});
 	}
+
+	#onclick = () => {
+		if (this.#isDisabled) return;
+		this.#root.toggleItem(this.#value.value);
+	};
+
+	#onkeydown = (e: KeyboardEvent) => {
+		if (this.#isDisabled) return;
+		if (e.key === kbd.ENTER || e.key === kbd.SPACE) {
+			e.preventDefault();
+			this.#root.toggleItem(this.#value.value);
+			return;
+		}
+		if (!this.#root.rovingFocus.value) return;
+
+		const node = this.#node.value;
+		if (!node) return;
+		const items = this.#root.getItemNodes();
+
+		const currentIndex = items.indexOf(node);
+
+		const dir = getElemDirection(node);
+		const { nextKey, prevKey } = getDirectionalKeys(dir, this.#root.orientation.value);
+
+		const keyToIndex = {
+			[nextKey]: currentIndex + 1,
+			[prevKey]: currentIndex - 1,
+			[kbd.HOME]: 0,
+			[kbd.END]: items.length - 1,
+		};
+
+		let itemIndex = keyToIndex[e.key];
+		if (itemIndex === undefined) return;
+		e.preventDefault();
+		const loop = this.#root.loop.value;
+
+		if (itemIndex < 0 && loop) {
+			itemIndex = items.length - 1;
+		} else if (itemIndex === items.length && loop) {
+			itemIndex = 0;
+		}
+
+		const itemToFocus = items[itemIndex];
+		if (!itemToFocus) return;
+		itemToFocus.focus();
+	};
+
+	isPressed() {
+		return this.#root.includesItem(this.#value.value);
+	}
+
+	getAriaPressed() {
+		return this.#root.isMulti
+			? getAriaPressed(this.#root.includesItem(this.#value.value))
+			: undefined;
+	}
+
+	getAriaChecked() {
+		return this.#root.isMulti
+			? undefined
+			: getAriaChecked(this.#root.includesItem(this.#value.value));
+	}
+
+	getTabIndex() {
+		if (this.#root.rovingFocus.value) {
+			return this.isPressed() ? 0 : -1;
+		}
+		return 0;
+	}
+
+	props = $derived({
+		id: this.#id.value,
+		role: this.#root.isMulti ? undefined : "radio",
+		tabindex: this.getTabIndex(),
+		"data-orientation": getDataOrientation(this.#root.orientation.value),
+		"data-disabled": getDataDisabled(this.#isDisabled),
+		"data-state": getToggleItemDataState(this.isPressed()),
+		"data-value": this.#value.value,
+		"aria-pressed": this.getAriaPressed(),
+		"aria-checked": this.getAriaChecked(),
+		[ITEM_ATTR]: "",
+		//
+		onclick: this.#onclick,
+		onkeydown: this.#onkeydown,
+	});
+}
+
+//
+// HELPERS
+//
+
+function getToggleItemDataState(condition: boolean) {
+	return condition ? "on" : "off";
+}
+
+//
+// CONTEXT METHODS
+//
+
+const TOGGLE_GROUP_ROOT_KEY = Symbol("ToggleGroup.Root");
+
+type InitToggleGroupProps = {
+	type: "single" | "multiple";
+	value: Box<string> | Box<string[]>;
+} & ReadonlyBoxedValues<{
+	id: string;
+	disabled: boolean;
+	rovingFocus: boolean;
+	loop: boolean;
+	orientation: Orientation;
+}>;
+
+export function setToggleGroupRootState(props: InitToggleGroupProps) {
+	const { type, ...rest } = props;
+	const rootState =
+		type === "single"
+			? new ToggleGroupSingleState(rest as ToggleGroupSingleStateProps)
+			: new ToggleGroupMultipleState(rest as ToggleGroupMultipleStateProps);
+	return setContext(TOGGLE_GROUP_ROOT_KEY, rootState);
+}
+
+export function getToggleGroupRootState(): ToggleGroupState {
+	verifyContextDeps(TOGGLE_GROUP_ROOT_KEY);
+	return getContext(TOGGLE_GROUP_ROOT_KEY);
+}
+
+export function setToggleGroupItemState(props: ToggleGroupItemStateProps) {
+	const rootState = getToggleGroupRootState();
+	return new ToggleGroupItemState({ ...props, rootState });
 }
