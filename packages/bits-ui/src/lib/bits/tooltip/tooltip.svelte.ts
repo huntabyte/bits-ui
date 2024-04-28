0524056ddeaa0869d9fdfca53a58994dd9efa739
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-27 21:12:19 -0400

    more

--- packages/bits-ui/src/lib/bits/tooltip/tooltip.svelte.ts
+++ packages/bits-ui/src/lib/bits/tooltip/tooltip.svelte.ts
@@ -1,6 +1,65 @@
 import { box } from "runed";
-import type { ReadableBoxedValues, WritableBoxedValues } from "$lib/internal/box.svelte.js";
+import { TOOLTIP_OPEN_EVENT } from "./utils.js";
+import {
+	type ReadableBoxedValues,
+	type WritableBoxedValues,
+	watch,
+} from "$lib/internal/box.svelte.js";
 import { useTimeoutFn } from "$lib/internal/useTimeoutFn.svelte.js";
+
+type TooltipProviderStateProps = ReadableBoxedValues<{
+	delayDuration: number;
+	disableHoverableContent: boolean;
+	disableCloseOnTriggerClick: boolean;
+	disabled: boolean;
+	ignoreNonKeyboardFocus: boolean;
+	skipDelayDuration: number;
+}>;
+
+class TooltipProviderState {
+	delayDuration: TooltipProviderStateProps["delayDuration"];
+	disableHoverableContent: TooltipProviderStateProps["disableHoverableContent"];
+	disableCloseOnTriggerClick: TooltipProviderStateProps["disableCloseOnTriggerClick"];
+	disabled: TooltipProviderStateProps["disabled"];
+	ignoreNonKeyboardFocus: TooltipProviderStateProps["ignoreNonKeyboardFocus"];
+	skipDelayDuration: TooltipProviderStateProps["skipDelayDuration"];
+	isOpenDelayed = box(true);
+	isPointerInTransit = box(false);
+	#timerFn: ReturnType<typeof useTimeoutFn>;
+
+	constructor(props: TooltipProviderStateProps) {
+		this.delayDuration = props.delayDuration;
+		this.disableHoverableContent = props.disableHoverableContent;
+		this.disableCloseOnTriggerClick = props.disableCloseOnTriggerClick;
+		this.disabled = props.disabled;
+		this.ignoreNonKeyboardFocus = props.ignoreNonKeyboardFocus;
+		this.skipDelayDuration = props.skipDelayDuration;
+		this.#timerFn = useTimeoutFn(
+			() => {
+				this.isOpenDelayed.value = true;
+			},
+			this.skipDelayDuration.value,
+			{ immediate: false }
+		);
+	}
+
+	#startTimer() {
+		this.#timerFn.start();
+	}
+
+	#clearTimer() {
+		this.#timerFn.stop();
+	}
+
+	onOpen() {
+		this.#clearTimer();
+		this.isOpenDelayed.value = false;
+	}
+
+	onClose() {
+		this.#startTimer();
+	}
+}
 
 type TooltipRootStateProps = ReadableBoxedValues<{
 	delayDuration: number;
@@ -15,31 +74,60 @@ type TooltipRootStateProps = ReadableBoxedValues<{
 
 class TooltipRootState {
 	open: TooltipRootStateProps["open"];
-	delayDuration: TooltipRootStateProps["delayDuration"];
-	disableHoverableContent: TooltipRootStateProps["disableHoverableContent"];
-	disableCloseOnTriggerClick: TooltipRootStateProps["disableCloseOnTriggerClick"];
-	disabled: TooltipRootStateProps["disabled"];
-	ignoreNonKeyboardFocus: TooltipRootStateProps["ignoreNonKeyboardFocus"];
+	_delayDuration: TooltipRootStateProps["delayDuration"];
+	_disableHoverableContent: TooltipRootStateProps["disableHoverableContent"];
+	_disableCloseOnTriggerClick: TooltipRootStateProps["disableCloseOnTriggerClick"];
+	_disabled: TooltipRootStateProps["disabled"];
+	_ignoreNonKeyboardFocus: TooltipRootStateProps["ignoreNonKeyboardFocus"];
+	provider: TooltipProviderState;
+	delayDuration = box.with(() => this._delayDuration.value ?? this.provider.delayDuration.value);
+	disableHoverableContent = box.with(
+		() => this._disableHoverableContent.value ?? this.provider.disableHoverableContent.value
+	);
+	disableCloseOnTriggerClick = box.with(
+		() =>
+			this._disableCloseOnTriggerClick.value ?? this.provider.disableCloseOnTriggerClick.value
+	);
+	disabled = box.with(() => this._disabled.value ?? this.provider.disabled.value);
+	ignoreNonKeyboardFocus = box.with(
+		() => this._ignoreNonKeyboardFocus.value ?? this.provider.ignoreNonKeyboardFocus.value
+	);
 	contentNode = box<HTMLElement | null>(null);
 	triggerNode = box<HTMLElement | null>(null);
 	#wasOpenDelayed = $state(false);
 	#timerFn: ReturnType<typeof useTimeoutFn>;
+	stateAttr = $derived.by(() => {
+		if (!this.open.value) return "closed";
+		return this.#wasOpenDelayed ? "delayed-open" : "instant-open";
+	});
 
-	constructor(props: TooltipRootStateProps) {
+	constructor(props: TooltipRootStateProps, provider: TooltipProviderState) {
+		this.provider = provider;
 		this.open = props.open;
-		this.delayDuration = props.delayDuration;
-		this.disableHoverableContent = props.disableHoverableContent;
-		this.disableCloseOnTriggerClick = props.disableCloseOnTriggerClick;
-		this.disabled = props.disabled;
-		this.ignoreNonKeyboardFocus = props.ignoreNonKeyboardFocus;
+		this._delayDuration = props.delayDuration;
+		this._disableHoverableContent = props.disableHoverableContent;
+		this._disableCloseOnTriggerClick = props.disableCloseOnTriggerClick;
+		this._disabled = props.disabled;
+		this._ignoreNonKeyboardFocus = props.ignoreNonKeyboardFocus;
 		this.#timerFn = useTimeoutFn(
 			() => {
 				this.#wasOpenDelayed = true;
 				this.open.value = true;
 			},
-			this.delayDuration.value,
+			this._delayDuration.value,
 			{ immediate: false }
 		);
+
+		watch(this.open, (isOpen) => {
+			if (!this.provider.onClose) return;
+			if (isOpen) {
+				this.provider.onOpen();
+
+				document.dispatchEvent(new CustomEvent(TOOLTIP_OPEN_EVENT));
+			} else {
+				this.provider.onClose();
+			}
+		});
 	}
 
 	handleOpen() {
@@ -62,7 +150,7 @@ class TooltipRootState {
 	}
 
 	onTriggerLeave() {
-		if (this.disableHoverableContent.value) {
+		if (this._disableHoverableContent.value) {
 			this.handleClose();
 		} else {
 			this.#timerFn.stop();
--- packages/bits-ui/src/lib/bits/tooltip/types.ts
+++ packages/bits-ui/src/lib/bits/tooltip/types.ts
@@ -11,6 +11,51 @@ import type {
 import type { PortalProps } from "$lib/bits/utilities/portal/types.js";
 import type { EventCallback } from "$lib/internal/events.js";
 
+export type TooltipProviderPropsWithoutHTML = {
+	/**
+	 * The delay in milliseconds before the tooltip opens.
+	 *
+	 * @defaultValue 700
+	 */
+	delayDuration?: number;
+
+	/**
+	 * How much time a user has to enter another trigger without
+	 * incurring a delay again.
+	 * @defaultValue 300
+	 */
+	skipDelayDuration?: number;
+
+	/**
+	 * Prevents tooltip from remaining open when hovering over the content.
+	 *
+	 * @defaultValue false
+	 */
+	disableHoverableContent?: boolean;
+
+	/**
+	 * When `true`, the tooltip will not close when you click on the trigger.
+	 *
+	 * @defaultValue false
+	 */
+	disableCloseOnTriggerClick?: boolean;
+
+	/**
+	 * When `true`, the tooltip will be disabled and will not open.
+	 *
+	 * @defaultValue false
+	 */
+	disabled?: boolean;
+
+	/**
+	 * Prevent the tooltip from opening if the focus did not come using
+	 * the keyboard.
+	 *
+	 * @defaultValue false
+	 */
+	ignoreNonKeyboardFocus?: boolean;
+};
+
 export type TooltipRootPropsWithoutHTML = {
 	/**
 	 * The open state of the tooltip.
