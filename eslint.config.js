18d5c442eeb5ff9cff88cc595275aa6a83375bc9
Hunter Johnston <johnstonhuntera@gmail.com>
2024-09-19 11:18:08 -0400

    cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ const ignores = ["**/extended-types"];
 export default config({
 	svelte: true,
 	ignores: [...DEFAULT_IGNORES, ...ignores],
-}).override("huntabyte:svelte:rules", {
+}).override("huntabyte/svelte/rules", {
 	// we ignore as it complains about the changed warning names in Svelte 5
 	rules: { "svelte/no-unused-svelte-ignore": "off" },
 });
