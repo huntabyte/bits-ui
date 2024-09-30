940f4dd15a29f7922844e236aac7b48d02158815
Hunter Johnston <johnstonhuntera@gmail.com>
2024-09-29 21:00:19 -0400

    improve demo styles o sink page

--- sites/docs/src/routes/(main)/sink/+page.svelte
+++ sites/docs/src/routes/(main)/sink/+page.svelte
@@ -1,33 +1,40 @@
 <script lang="ts">
 	import { TagsInput } from "bits-ui";
 	import X from "phosphor-svelte/lib/X";
-	import DropdownMenuDemo from "$lib/components/demos/dropdown-menu-demo.svelte";
 
 	let value = $state<string[]>(["hello", "world"]);
 </script>
 
 <div class="flex items-center justify-center">
-	<TagsInput.Root
-		bind:value
-		class="inline-flex h-input w-full items-center gap-4 rounded-card-sm border border-border-input bg-background px-4 text-sm placeholder:text-foreground-alt/50 hover:border-dark-40 focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-background"
-	>
-		<TagsInput.List class="flex flex-wrap gap-1.5">
-			{#each value as tag}
-				<TagsInput.Tag
-					value={tag}
-					class="ml-2 flex items-center gap-1 rounded-[4px] bg-[#FCDAFE] px-1.5 py-1 text-[0.7rem] font-semibold leading-none text-[#2A266B] no-underline group-hover:no-underline"
-				>
-					<TagsInput.TagContent>
-						{tag}
-					</TagsInput.TagContent>
-					<TagsInput.TagRemove>
-						<X class="size-3" />
-					</TagsInput.TagRemove>
-				</TagsInput.Tag>
-			{/each}
-		</TagsInput.List>
-		<TagsInput.Input class="bg-transparent" placeholder="Add a tag..." />
-		<TagsInput.Clear class="ml-auto flex items-center gap-1">
+	<TagsInput.Root bind:value class="flex flex-col gap-2">
+		<div
+			class="flex h-auto w-[400px] flex-col gap-4 rounded-card-sm border border-border-input bg-background p-4 text-sm placeholder:text-foreground-alt/50 focus-within:outline-none focus-within:ring-2 focus-within:ring-foreground focus-within:ring-offset-2 focus-within:ring-offset-background hover:border-dark-40"
+		>
+			<TagsInput.List class="flex flex-wrap gap-1.5">
+				{#each value as tag}
+					<TagsInput.Tag
+						value={tag}
+						class="ml-2 flex items-center gap-1 rounded-[4px] bg-[#FCDAFE] px-1.5 py-1 text-[0.7rem] font-semibold leading-none text-[#2A266B] no-underline group-hover:no-underline"
+					>
+						<TagsInput.TagContent>
+							{tag}
+						</TagsInput.TagContent>
+						<TagsInput.TagRemove>
+							<X class="size-3" />
+						</TagsInput.TagRemove>
+					</TagsInput.Tag>
+				{/each}
+			</TagsInput.List>
+			<TagsInput.Input
+				class="focus-override bg-transparent focus:outline-none focus:ring-0 "
+				placeholder="Add a tag..."
+			/>
+		</div>
+		<TagsInput.Clear
+			class="inline-flex h-9 items-center
+	justify-center whitespace-nowrap rounded-input bg-dark px-[21px]
+	text-[15px] font-semibold text-background shadow-mini transition-colors hover:bg-dark/95 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
+		>
 			Clear
 			<X class="size-5" />
 		</TagsInput.Clear>
