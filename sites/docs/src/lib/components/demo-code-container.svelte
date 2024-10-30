c12b9ea3c081553c4cd81e4495fbdde073c1194a
Pavla Novotná <82083401+pavlanovotna@users.noreply.github.com>
2024-10-30 14:36:29 +0100

    next: page scrollbars (#842)
    Co-authored-by: Pavla Novotná <pavla.novotna@pslib.cz>

--- sites/docs/src/lib/components/demo-code-container.svelte
+++ sites/docs/src/lib/components/demo-code-container.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
-	import { Collapsible, Tabs } from "bits-ui";
+	import { Collapsible, ScrollArea, Tabs } from "bits-ui";
 	import DemoCodeTabs from "./demo-code-tabs.svelte";
 	import TailwindConfig from "./code-renders/tailwind-config.svelte";
 	import AppCSS from "./code-renders/app-css.svelte";
@@ -56,21 +56,43 @@
 				class="relative overflow-hidden rounded-b-card border-x-2 border-b-2 bg-background"
 			>
 				<Collapsible.Content forceMount>
-					<div
-						class={cn(
-							"w-full [&_pre]:!my-0 [&_pre]:!mt-0 [&_pre]:max-h-[800px] [&_pre]:min-h-80 [&_pre]:overflow-auto [&_pre]:!rounded-none [&_pre]:!rounded-b-none [&_pre]:!rounded-tl-none [&_pre]:!rounded-tr-none [&_pre]:!border-t-0 [&_pre]:!border-none [&_pre]:!px-2 [&_pre]:!pb-5 [&_pre]:!pt-2",
-							!open && "[&_pre]:!max-h-80",
-							className
-						)}
-					>
-						{#if item.value === fileName}
-							{@render children()}
-						{:else if item.value === "tailwind.config.js"}
-							<TailwindConfig />
-						{:else if item.value === "app.css"}
-							<AppCSS />
-						{/if}
-					</div>
+					<ScrollArea.Root>
+						<ScrollArea.Viewport
+							class={cn(
+								"h-full max-h-[800px] min-h-80 w-full",
+								!open && "!max-h-80",
+								className
+							)}
+						>
+							<div
+								class={cn(
+									"w-full [&_pre]:!my-0 [&_pre]:!mt-0 [&_pre]:!rounded-none [&_pre]:!rounded-b-none [&_pre]:!rounded-tl-none [&_pre]:!rounded-tr-none [&_pre]:!border-t-0 [&_pre]:!border-none [&_pre]:!px-2 [&_pre]:!pb-5 [&_pre]:!pt-2",
+									className
+								)}
+							>
+								{#if item.value === fileName}
+									{@render children()}
+								{:else if item.value === "tailwind.config.js"}
+									<TailwindConfig />
+								{:else if item.value === "app.css"}
+									<AppCSS />
+								{/if}
+							</div>
+						</ScrollArea.Viewport>
+						<ScrollArea.Scrollbar
+							orientation="vertical"
+							class="flex w-2.5 touch-none select-none rounded-full border-l border-l-transparent bg-transparent p-px transition-all duration-200 hover:w-3 hover:bg-dark-10 data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out-0 data-[state=visible]:fade-in-0"
+						>
+							<ScrollArea.Thumb class="flex-1 rounded-full bg-muted-foreground" />
+						</ScrollArea.Scrollbar>
+						<ScrollArea.Scrollbar
+							orientation="horizontal"
+							class="flex h-2.5 touch-none select-none rounded-full border-t border-t-transparent bg-transparent p-px transition-all duration-200 hover:h-3 hover:bg-dark-10 "
+						>
+							<ScrollArea.Thumb class="rounded-full bg-muted-foreground" />
+						</ScrollArea.Scrollbar>
+						<ScrollArea.Corner />
+					</ScrollArea.Root>
 				</Collapsible.Content>
 			</Tabs.Content>
 		{/each}
