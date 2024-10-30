f36c5e412dd8088d50f5a25053a796f85a29b1c9
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-30 17:09:51 -0400

    fix: combobox forcemount

--- sites/docs/src/lib/components/demos/index.ts
+++ sites/docs/src/lib/components/demos/index.ts
@@ -11,6 +11,7 @@ export { default as CheckboxDemoCustom } from "./checkbox-demo-custom.svelte";
 export { default as CollapsibleDemo } from "./collapsible-demo.svelte";
 export { default as CollapsibleDemoTransitions } from "./collapsible-demo-transitions.svelte";
 export { default as ComboboxDemo } from "./combobox-demo.svelte";
+export { default as ComboboxDemoTransition } from "./combobox-demo-transition.svelte";
 export { default as CommandDemo } from "./command-demo.svelte";
 export { default as CommandDemoDialog } from "./command-demo-dialog.svelte";
 export { default as ContextMenuDemo } from "./context-menu-demo.svelte";
