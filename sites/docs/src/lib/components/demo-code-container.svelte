db9dc9a550244986e97843460ee81a44398be3be
Pavla Novotná <pavla.novotna@pslib.cz>
2024-10-30 10:26:50 +0100

    demo code bits scrollbar

--- sites/docs/src/lib/components/demo-code-container.svelte
+++ sites/docs/src/lib/components/demo-code-container.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
-	import { Collapsible, Tabs } from "bits-ui";
+	import { Collapsible, ScrollArea , Tabs } from "bits-ui";
 	import DemoCodeTabs from "./demo-code-tabs.svelte";
 	import TailwindConfig from "./code-renders/tailwind-config.svelte";
 	import AppCSS from "./code-renders/app-css.svelte";
 	import { cn } from "$lib/utils/styles.js";
-
+  
 	type Props = {
 		fileName?: string;
 		children: Snippet;
@@ -56,10 +56,15 @@
 				class="relative overflow-hidden rounded-b-card border-x-2 border-b-2 bg-background"
 			>
 				<Collapsible.Content forceMount>
-					<div
+          <ScrollArea.Root>
+            <ScrollArea.Viewport class={cn(
+							"w-full max-h-[800px] min-h-80 h-full",
+							!open && "!max-h-80",
+							className
+						)}>
+            <div
 						class={cn(
-							"w-full [&_pre]:!my-0 [&_pre]:!mt-0 [&_pre]:max-h-[800px] [&_pre]:min-h-80 [&_pre]:overflow-auto [&_pre]:!rounded-none [&_pre]:!rounded-b-none [&_pre]:!rounded-tl-none [&_pre]:!rounded-tr-none [&_pre]:!border-t-0 [&_pre]:!border-none [&_pre]:!px-2 [&_pre]:!pb-5 [&_pre]:!pt-2",
-							!open && "[&_pre]:!max-h-80",
+							"w-full [&_pre]:!my-0 [&_pre]:!mt-0 [&_pre]:!rounded-none [&_pre]:!rounded-b-none [&_pre]:!rounded-tl-none [&_pre]:!rounded-tr-none [&_pre]:!border-t-0 [&_pre]:!border-none [&_pre]:!px-2 [&_pre]:!pb-5 [&_pre]:!pt-2",
 							className
 						)}
 					>
@@ -71,6 +76,22 @@
 							<AppCSS />
 						{/if}
 					</div>
+          </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+              orientation="vertical"
+              class="flex w-2.5 touch-none select-none rounded-full border-l border-l-transparent bg-transparent p-px transition-all duration-200 hover:w-3 hover:bg-dark-10 data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out-0 data-[state=visible]:fade-in-0"
+              >
+              <ScrollArea.Thumb class="flex-1 rounded-full bg-muted-foreground" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Scrollbar
+              orientation="horizontal"
+              class="flex h-2.5 touch-none select-none rounded-full border-t border-t-transparent bg-transparent p-px transition-all duration-200 hover:h-3 hover:bg-dark-10 "
+            >
+              <ScrollArea.Thumb class="rounded-full bg-muted-foreground" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner />
+          </ScrollArea.Root>
+					
 				</Collapsible.Content>
 			</Tabs.Content>
 		{/each}
--- sites/docs/src/lib/components/navigation/sidebar-nav.svelte
+++ sites/docs/src/lib/components/navigation/sidebar-nav.svelte
@@ -44,30 +44,4 @@
       <ScrollArea.Corner />
     </ScrollArea.Root>
   </aside>
-	<!-- <aside
-		class="fixed top-10 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-border md:sticky md:block"
-	>
-		<div class="h-full py-6 pr-4 lg:py-8">
-			<nav class="space-y-3">
-				<div class="flex w-full flex-col pb-[50px]">
-					{#each items as item, index (index)}
-						{#if item.title === "Overview"}
-							<SidebarNavMainItems items={item.items} />
-						{:else}
-							<div class="pb-4">
-								<h4
-									class="mb-1 ml-[9px] rounded-md px-2.5 py-2 pl-4 text-xs font-medium uppercase text-muted-foreground"
-								>
-									{item.title}
-								</h4>
-								{#if item.items}
-									<SidebarNavItems items={item.items} />
-								{/if}
-							</div>
-						{/if}
-					{/each}
-				</div>
-			</nav>
-		</div>
-	</aside> -->
 {/if}
