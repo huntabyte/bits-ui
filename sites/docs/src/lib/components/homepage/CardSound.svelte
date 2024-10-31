0485f67e6bedc169811845dd8b57d67257447312
Pavla Novotná <pavla.novotna@pslib.cz>
2024-10-31 11:13:47 +0100

    homepage top text styling & base of showcase components

---
+++ sites/docs/src/lib/components/homepage/CardSound.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import HomeToolbar from "$lib/components/homepage/HomeToolbar.svelte";
+</script>
+
+<div class="bg-foreground rounded-card-lg py-3 px-[14px]">
+  <div class="flex justify-between">
+    <h3 class="text-background text-xl font-medium ml-[6px] mt-[7px]">
+      <span class="">Sound</span>
+      <small class="opacity-50 block text-xs">control</small>
+    </h3>
+    <HomeToolbar/>
+  </div>
+
+  <div class="sound_meter bg-background relative">
+    <div class="sound_meter__toggle bg-rose-500 w-2 h-[26px] rounded-card-sm"></div>
+    <div class="sound_meter__labels">
+      <div class="sound_meter__label label_left">L</div>
+      <div class="sound_meter__label label_right">R</div>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .sound_meter{
+    width: 100%;
+    aspect-ratio: 1/1;
+    border-radius: 50%;   
+    margin-top: 17px; 
+  }
+  .sound_meter__toggle{
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .sound_meter__labels{
+    position: absolute;
+    top: 50%;
+    left: 8px;
+    right: 8px;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .sound_meter__label{
+    color: #17171766;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    &.label_left{
+      &:before{
+        content: "";
+        display: inline-block;
+        width: 7px;
+        height: 7px;
+        border-radius: 50%;
+        aspect-ratio: 1/1;
+        background-color: #CBCBCBB2;
+        
+      }
+    }
+    &.label_right{
+      &:after{
+        content: "";
+        display: inline-block;
+        width: 7px;
+        height: 7px;
+        border-radius: 50%;
+        aspect-ratio: 1/1;
+        background-color: #CBCBCBB2;
+        
+      }
+    }
+  }
+</style>
---
+++ sites/docs/src/routes/homepage/+page.svelte
@@ -0,0 +1,145 @@
+<script lang="ts">
+	import "$lib/styles/app.postcss";
+	import { Button, Tabs } from "bits-ui";
+	import Sparkle from "phosphor-svelte/lib/Sparkle";
+  import CardSound from "$lib/components/homepage/CardSound.svelte";
+	
+	import { Logo } from "$icons/index.js";
+	import LightSwitch from "$lib/components/light-switch.svelte";
+  import HomeSlider from "$lib/components/homepage/HomeSlider.svelte";
+  import HomeCombobox from "$lib/components/homepage/HomeCombobox.svelte";
+  import HomeSwitch from "$lib/components/homepage/HomeSwitch.svelte";
+  
+	</script>
+
+<header class="grid grid-cols-3 px-5 py-[17px]">
+	<a
+		href="/"
+		class="ml-2 mt-2 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background"
+	>
+		<Logo />
+	</a>
+
+	<nav class="mt-2 text-center">
+		<ul>
+			<li class="active"><a href="/">Home</a></li>
+			<li><a href="/">Showcase</a></li>
+			<li><a href="/">Docs</a></li>
+		</ul>
+	</nav>
+
+	<div class="flex justify-end gap-3">
+		<LightSwitch />
+
+		<Button.Root
+			class="inline-flex h-10 items-center justify-center rounded-input bg-dark
+      px-5 text-[14px] font-semibold text-background shadow-mini
+      hover:bg-dark/95 active:scale-98 active:transition-all"
+		>
+			Get started
+		</Button.Root>
+	</div>
+</header>
+
+<section>
+	<div class="mx-auto w-1/2 text-center">
+		<a
+			href="/"
+			class="inline-flex gap-2 rounded-card-lg border border-border-input py-1.5 pl-3 pr-4 shadow-mini"
+		>
+			<Sparkle class="size-4" aria-label="Stars" />
+			<span
+				class="inline-block border-l border-l-border-input pl-3 text-[13px] font-medium text-foreground"
+				>New Svelte 5 version</span
+			>
+		</a>
+		<h1>Build your own amazing UI elements</h1>
+		<p class="mx-auto w-9/12 font-medium text-muted-foreground">
+			Bits UI is a collection of headless component primitives that enable you to build your
+			own custom components.
+		</p>
+		<Button.Root
+			class="inline-flex h-10 items-center justify-center rounded-input bg-dark
+      px-3 shadow-mini
+      hover:bg-dark/95 active:scale-98 active:transition-all"
+		>
+			<kbd
+				class="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border bg-background-alt px-1.5 font-mono text-[10px] font-medium opacity-100 shadow-kbd sm:flex"
+			>
+				<span class="text-xs">S</span>
+			</kbd>
+			<span class="ml-4 mr-3 text-[15px] font-semibold text-background">Start building</span>
+		</Button.Root>
+	</div>
+
+  <div class="grid grid-cols-4">
+    
+      <CardSound/>
+    
+    <div class="border">
+      <HomeSlider/>
+    </div>
+    <div class="border">
+      <Tabs.Root value="follow">
+        <Tabs.List>
+          <Tabs.Trigger value="follow" class="">Follow</Tabs.Trigger>
+          <Tabs.Trigger value="other" class="">Other</Tabs.Trigger>
+        </Tabs.List>
+    
+        <Tabs.Content value="follow" class="select-none">follow</Tabs.Content>
+        <Tabs.Content value="other" class="select-none">other</Tabs.Content>
+      </Tabs.Root>
+    </div>
+    <div class="border">
+      <HomeCombobox/>
+      <HomeSwitch/>
+    </div>
+  </div>
+
+
+  
+</section>
+
+<style lang="postcss">
+	header {
+		nav {
+			li {
+				display: inline-block;
+				a {
+					font-size: 14px;
+					letter-spacing: 0.01em;
+					line-height: 1;
+					font-weight: 500;
+					-webkit-font-smoothing: antialiased;
+					border-radius: 40px;
+					padding: 0.3em 0.9em 0.4em 0.9em;
+					color: hsl(var(--foreground) / 0.5);
+				}
+				&.active {
+					a {
+						background: rgba(0, 0, 0, 0.05);
+						color: hsl(var(--foreground));
+					}
+				}
+			}
+		}
+	}
+
+	section {
+		height: calc(100svh - 76px); /* header height */
+		min-height: 700px;
+		padding-top: 7em;
+	}
+	h1 {
+		text-wrap: balance;
+		font-size: clamp(3.125rem, 3.6vw + 1rem, 5rem);
+		line-height: 102%;
+		margin-bottom: 0.35em;
+		margin-top: 0.2em;
+	}
+	p {
+		font-size: 1.125rem;
+		line-height: 144%;
+		margin-bottom: 2em;
+	}
+</style>
