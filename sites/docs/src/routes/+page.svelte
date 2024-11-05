585aa128aba076ee397a2e1d7200b1c690082493
Pavla Novotná <pavla.novotna@pslib.cz>
2024-11-05 11:18:45 +0100

    bottom section desktop

--- sites/docs/src/routes/+page.svelte
+++ sites/docs/src/routes/+page.svelte
@@ -2,12 +2,19 @@
 	import "$lib/styles/app.postcss";
 	import { Button } from "bits-ui";
 	import Sparkle from "phosphor-svelte/lib/Sparkle";
+  import SlidersHorizontal from "phosphor-svelte/lib/SlidersHorizontal";
+  import Keyboard from "phosphor-svelte/lib/Keyboard";
+  import Stack from "phosphor-svelte/lib/Stack";
 	import CardAir from "$lib/components/homepage/card-air.svelte";
 	import CardSound from "$lib/components/homepage/card-sound.svelte";
 	import CardTimer from "$lib/components/homepage/card-timer.svelte";
 	import CardToggle from "$lib/components/homepage/card-toggle.svelte";
 	import { Logo } from "$icons/index.js";
 	import LightSwitch from "$lib/components/light-switch.svelte";
+
+  import Customizable from "$lib/components/homepage/svg-customizable.svelte";
+  import Accessible from "$lib/components/homepage/svg-accessible.svelte";
+  import Consistent from "$lib/components/homepage/svg-consistent.svelte";
 </script>
 
 <div class="border-b border-solid border-black/10 bg-[#f5f5f5]/30">
@@ -75,7 +82,7 @@
 				>
 			</a>
 			<h1
-				class="mb-[0.35em] mt-[0.2em] text-balance pb-[0.1em] text-[4.25rem] font-semibold leading-[102%] antialiased !text-foreground"
+				class="font-['Cal_Sans'] mb-[0.35em] mt-[0.2em] text-balance pb-[0.1em] text-[4.25rem] font-semibold leading-[102%] antialiased text-transparent selection:text-foreground"
 			>
 				The headless components for Svelte
 			</h1>
@@ -92,7 +99,7 @@
 				href="/docs"
 			>
 				<kbd
-					class="shadow-kbd_homepage pointer-events-none hidden h-5 select-none items-center gap-1 rounded border bg-background-alt px-1.5 font-mono text-xxs font-medium opacity-100 sm:flex"
+					class="shadow-[0px_2px_0px_0px_#59595b] pointer-events-none hidden h-5 select-none items-center gap-1 rounded border bg-background-alt px-1.5 font-mono text-xxs font-medium opacity-100 sm:flex"
 				>
 					<span class="text-xs text-foreground-alt">S</span>
 				</kbd>
@@ -119,18 +126,82 @@
 	</section>
 </div>
 
+<section class="w-10/12 mx-auto mt-20 mb-44">
+  <h2 class=" text-foreground w-1/3 text-balance mb-10">
+    <small class="border border-foreground rounded-3xl text-[13px] tracking-[0.01em] px-2 py-[3px] font-medium"><span class="inline-block w-[10px] h-[10px] rounded-full bg-foreground/90 mr-3"/>Explore</small>
+    <span class="block tracking-[0.02em] text-[32px] font-semibold font-['Cal_Sans'] leading-[118%] mt-2">Main benefits of using our headless collection</span>
+  </h2>
+  <div class="grid grid-cols-3 gap-x-[15px] ">
+
+    <div class="border border-border rounded-card-lg  shadow-card overflow-hidden">
+      <div class="bg-[#FFF6F5] rounded-15px mx-2 my-[10px] pl-6 pr-2 pt-2 pb-5">
+        <div class="flex items-center justify-between ">
+          <h3 class="font-semibold text-[17px] -tracking-[0.01em]">Customizable</h3>
+          <div class="inline-flex w-[50px] h-[50px] rounded-full items-center justify-center bg-foreground">
+            <SlidersHorizontal class="text-background size-6"/>
+          </div>
+        </div>
+  
+        <div class="pt-9 pb-16">
+          <div class="relative w-full aspect-[388/261] bg-transparent left-[17px]">
+            <Customizable />
+          </div>
+        </div>
+
+        <div class="font-medium text-sm text-muted-foreground">Wide range of props for customizing </div>
+      </div>
+    </div>
+
+    <div class="rounded-card-lg  shadow-card overflow-hidden">
+      <div class="bg-[#FEFCE8] rounded-15px pl-8 pr-4 pt-[18px] pb-[30px]">
+        <div class="flex items-center justify-between ">
+          <h3 class="font-semibold text-[17px] -tracking-[0.01em]">Accessible</h3>
+          <div class="inline-flex w-[50px] h-[50px] rounded-full items-center justify-center bg-foreground">
+            <Keyboard class="text-background size-6"/>
+          </div>
+        </div>
+  
+        <div class="pt-9 pb-16">
+          <div class="relative w-full aspect-[388/261] bg-transparent left-[17px]">
+            <Accessible />
+          </div>
+        </div>
+
+        <div class="font-medium text-sm text-muted-foreground">Components are accessible by default</div>
+      </div>
+    </div>
+
+    <div class="rounded-card-lg  shadow-card overflow-hidden">
+      <div class="bg-foreground rounded-15px pl-8 pr-4 pt-[18px] pb-[30px]">
+        <div class="flex items-center justify-between ">
+          <h3 class="font-semibold text-[17px] -tracking-[0.01em] text-background">Consistent</h3>
+          <div class="inline-flex w-[50px] h-[50px] rounded-full items-center justify-center bg-background">
+            <Stack class="text-foreground size-6"/>
+          </div>
+        </div>
+  
+        <div class="pt-9 pb-16">
+          <div class="relative w-full aspect-[388/261] bg-transparent left-[17px]">
+            <Consistent />
+          </div>
+        </div>
+
+        <div class="font-medium text-sm text-background/40">Components are as intuitive as possible</div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <style lang="postcss">
 
 	h1 {
-		font-family: "Cal Sans", sans-serif;
 		background: linear-gradient(
 				180deg,
 				rgba(255, 255, 255, 0.1) 0%,
 				rgba(255, 255, 255, 0) 41.43%
 			),
 			#272727;
-		-webkit-background-clip: text;
-		-webkit-text-fill-color: transparent;
+    background-clip: text;
     font-weight: 600;
 	}
 
@@ -186,7 +257,4 @@
 		}
 	}
 
-	.shadow-kbd_homepage {
-		box-shadow: 0px 2px 0px 0px #59595b;
-	}
 </style>
