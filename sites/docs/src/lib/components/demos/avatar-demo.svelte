a718121ab25d2a51617ebd3ce10b86c9f2e270c8
Hunter Johnston <johnstonhuntera@gmail.com>
2024-11-14 16:53:17 -0500

    docs: cleanup demo

--- sites/docs/src/lib/components/demos/avatar-demo.svelte
+++ sites/docs/src/lib/components/demos/avatar-demo.svelte
@@ -2,10 +2,8 @@
 	import { Avatar } from "bits-ui";
 
 	let loadingStatus = $state<Avatar.RootProps["loadingStatus"]>("loading");
-	let src = $state<string | null>("/avatar-1.png");
 </script>
 
-<button onclick={() => (src = null)}> Remove src </button>
 <Avatar.Root
 	bind:loadingStatus
 	class="h-12 w-12 rounded-full border {loadingStatus === 'loaded'
@@ -15,7 +13,7 @@
 	<div
 		class="flex h-full w-full items-center justify-center overflow-hidden rounded-full border-2 border-transparent"
 	>
-		<Avatar.Image {src} alt="@huntabyte" />
+		<Avatar.Image src="/avatar-1.png" alt="@huntabyte" />
 		<Avatar.Fallback class="border border-muted">HB</Avatar.Fallback>
 	</div>
 </Avatar.Root>
