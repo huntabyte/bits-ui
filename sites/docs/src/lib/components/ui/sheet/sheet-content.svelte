ffc5db70c536628a26172bc49baeeb9ca3d0bd71
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-11-15 19:47:47 -0500

    fix: dialog buttons not responding on certain mobile platforms (#937)

--- sites/docs/src/lib/components/ui/sheet/sheet-content.svelte
+++ sites/docs/src/lib/components/ui/sheet/sheet-content.svelte
@@ -1,21 +1,43 @@
+<script lang="ts" module>
+	import { type VariantProps, tv } from "tailwind-variants";
+
+	export const sheetVariants = tv({
+		base: "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
+		variants: {
+			side: {
+				top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
+				bottom: "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
+				left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+				right: "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+			},
+		},
+		defaultVariants: {
+			side: "right",
+		},
+	});
+
+	export type Side = VariantProps<typeof sheetVariants>["side"];
+</script>
+
 <script lang="ts">
-	import { Dialog as SheetPrimitive } from "bits-ui";
+	import { Dialog as SheetPrimitive, type WithoutChildrenOrChild } from "bits-ui";
 	import X from "phosphor-svelte/lib/X";
 	import type { Snippet } from "svelte";
-	import { SheetOverlay, SheetPortal, type Side, sheetVariants } from "./index.js";
+	import SheetOverlay from "./sheet-overlay.svelte";
 	import { cn } from "$lib/utils/index.js";
 
 	let {
-		side,
+		side = "right",
 		class: className,
 		children,
 		...restProps
-	}: Omit<SheetPrimitive.ContentProps & { side?: Side }, "child" | "children" | "ref"> & {
+	}: WithoutChildrenOrChild<SheetPrimitive.ContentProps> & {
 		children?: Snippet;
+		side?: Side;
 	} = $props();
 </script>
 
-<SheetPortal>
+<SheetPrimitive.Portal>
 	<SheetOverlay />
 	<SheetPrimitive.Content class={cn(sheetVariants({ side }), className)} {...restProps}>
 		{@render children?.()}
@@ -26,4 +48,4 @@
 			<span class="sr-only">Close</span>
 		</SheetPrimitive.Close>
 	</SheetPrimitive.Content>
-</SheetPortal>
+</SheetPrimitive.Portal>
