ca18009245ce9e1b05d0e30d676dc2f3790cac89
Aidan Bleser <117548273+ieedan@users.noreply.github.com>
2025-01-18 18:17:28 -0600

    feat: Allow `<Button/>` with `href` to be disabled (#1055)

--- sites/docs/src/lib/content/api-reference/button.api.ts
+++ sites/docs/src/lib/content/api-reference/button.api.ts
@@ -1,8 +1,11 @@
 import type { ButtonRootPropsWithoutHTML } from "bits-ui";
 import { childrenSnippet, createApiSchema, createPropSchema, refProp } from "./helpers.js";
 import * as C from "$lib/content/constants.js";
+import type { HTMLButtonAttributes } from "svelte/elements";
 
-export const root = createApiSchema<ButtonRootPropsWithoutHTML & { href: string }>({
+export const root = createApiSchema<
+	ButtonRootPropsWithoutHTML & { href: string; disabled: HTMLButtonAttributes["disabled"] }
+>({
 	title: "Root",
 	description:
 		"A component that can switch between a button and an anchor tag based on the `href`/`type` props.",
@@ -11,6 +14,12 @@ export const root = createApiSchema<ButtonRootPropsWithoutHTML & { href: string 
 			type: C.STRING,
 			description:
 				"An optional prop that when passed converts the button into an anchor tag.",
+		}),
+		disabled: createPropSchema({
+			type: C.BOOLEAN,
+			description:
+				"Whether or not the button is disabled. When disabled, the button cannot be interacted with.",
+			default: "false",
 		}),
 		ref: refProp({ elType: "HTMLButtonElement" }),
 		children: childrenSnippet(),
