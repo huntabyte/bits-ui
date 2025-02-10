a45bee142ed145c96b533c737671eda2fd6e7bcd
Hunter Johnston <johnstonhuntera@gmail.com>
2025-02-10 18:47:41 -0500

    fix: lint

--- packages/tests/src/tests/context-menu/context-menu.test.ts
+++ packages/tests/src/tests/context-menu/context-menu.test.ts
@@ -1,4 +1,4 @@
-import { cleanup, render, screen, waitFor } from "@testing-library/svelte/svelte5";
+import { render, screen, waitFor } from "@testing-library/svelte/svelte5";
 import { axe } from "jest-axe";
 import { describe, it } from "vitest";
 import { getTestKbd, setupUserEvents } from "../utils.js";
