ed79ef5615de32602dc0af06238f80d047b06add
Hunter Johnston <johnstonhuntera@gmail.com>
2025-02-17 14:48:48 -0500

    add test for fix

---
+++ tests/src/tests/radio-group/radio-group-popover-test.svelte
@@ -0,0 +1,44 @@
+<script lang="ts" module>
+	import { RadioGroup } from "bits-ui";
+	export type Item = {
+		value: string;
+		disabled: boolean;
+	};
+
+	export type RadioGroupTestProps = Omit<
+		RadioGroup.RootProps,
+		"child" | "children" | "asChild"
+	> & {
+		items: Item[];
+	};
+</script>
+
+<script lang="ts">
+	import { Popover } from "bits-ui";
+
+	let { items, value = "", ...restProps }: RadioGroupTestProps = $props();
+</script>
+
+<main>
+	<Popover.Root>
+		<Popover.Trigger data-testid="trigger">open</Popover.Trigger>
+		<Popover.Content data-testid="content">
+			<RadioGroup.Root data-testid="root" bind:value {...restProps}>
+				{#each items as { value, disabled }}
+					<RadioGroup.Item id={value} {value} {disabled} data-testid="{value}-item">
+						{#snippet children({ checked })}
+							<span data-testid="{value}-indicator"> {checked} </span>
+							{value}
+						{/snippet}
+					</RadioGroup.Item>
+					<label for={value} data-testid="{value}-label">
+						Label for {value}
+					</label>
+				{/each}
+			</RadioGroup.Root>
+		</Popover.Content>
+	</Popover.Root>
+	<div data-testid="value">
+		{value}
+	</div>
+</main>
