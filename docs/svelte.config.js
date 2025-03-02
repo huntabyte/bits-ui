04b030fc2c3b61bf09cc860771b965856f067765
Hunter Johnston <johnstonhuntera@gmail.com>
2025-03-01 20:23:19 -0500

    remove melt

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsx } from "mdsx";
-import { preprocessMeltUI } from "@melt-ui/pp";
 import { mdsxConfig } from "./mdsx.config.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [mdsx(mdsxConfig), vitePreprocess(), preprocessMeltUI()],
+	preprocess: [mdsx(mdsxConfig), vitePreprocess()],
 	extensions: [".svelte", ".md"],
 
 	kit: {
