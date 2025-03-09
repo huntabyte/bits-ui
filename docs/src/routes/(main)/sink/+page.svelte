b99fb6b7baa9cd9ffa8bdb6e7f8713777c78fe11
Hunter Johnston <johnstonhuntera@gmail.com>
2025-03-08 22:20:31 -0500

    month and year selects

--- docs/src/routes/(main)/sink/+page.svelte
+++ docs/src/routes/(main)/sink/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import DemoContainer from "$lib/components/demo-container.svelte";
-	import AccordionDemoCheckoutSteps from "$lib/components/demos/accordion-demo-checkout-steps.svelte";
+	import CalendarDemoSelects from "$lib/components/demos/calendar-demo-selects.svelte";
 </script>
 
 <div class="w-full max-w-[756px]">
 	<DemoContainer>
-		<AccordionDemoCheckoutSteps />
+		<CalendarDemoSelects />
 	</DemoContainer>
 </div>
