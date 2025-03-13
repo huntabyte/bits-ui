46a2cac045a86c2309989c1066eaba0af23709ad
Hunter Johnston <johnstonhuntera@gmail.com>
2025-03-12 21:18:01 -0400

    fix(Command): ensure original order is properly restored when search is cleared

--- packages/bits-ui/src/lib/bits/command/command.svelte.ts
+++ packages/bits-ui/src/lib/bits/command/command.svelte.ts
@@ -107,6 +107,9 @@ class CommandRootState {
 			this.#filterItems();
 			this.#sort();
 			this.#selectFirstItem();
+			afterTick(() => {
+				this.#selectFirstItem();
+			});
 		} else if (key === "value") {
 			// opts is a boolean referring to whether it should NOT be scrolled into view
 			if (!opts) {
@@ -336,17 +339,18 @@ class CommandRootState {
 	 * Special handling for first items in groups.
 	 */
 	#scrollSelectedIntoView(): void {
-		afterSleep(1, () => {
+		afterTick(() => {
 			const item = this.#getSelectedItem();
 			if (!item) return;
 			const grandparent = item.parentElement?.parentElement;
 			if (!grandparent) return;
 			const firstChildOfParent = getFirstNonCommentChild(grandparent) as HTMLElement | null;
 			if (firstChildOfParent && firstChildOfParent.dataset?.value === item.dataset?.value) {
-				item
+				const closestGroupHeader = item
 					?.closest(COMMAND_GROUP_SELECTOR)
-					?.querySelector(COMMAND_GROUP_HEADING_SELECTOR)
-					?.scrollIntoView({ block: "nearest" });
+					?.querySelector(COMMAND_GROUP_HEADING_SELECTOR);
+				closestGroupHeader?.scrollIntoView({ block: "nearest" });
+
 				return;
 			}
 			item.scrollIntoView({ block: "nearest" });
@@ -497,16 +501,18 @@ class CommandRootState {
 
 		this.#scheduleUpdate();
 		return () => {
+			const selectedItem = this.#getSelectedItem();
 			this.allIds.delete(id);
 			this.allItems.delete(id);
 			this.commandState.filtered.items.delete(id);
-			const selectedItem = this.#getSelectedItem();
 
 			this.#filterItems();
 
 			// The item removed have been the selected one,
 			// so selection should be moved to the first
-			if (selectedItem?.getAttribute("id") === id) this.#selectFirstItem();
+			if (selectedItem?.getAttribute("id") === id) {
+				this.#selectFirstItem();
+			}
 
 			this.#scheduleUpdate();
 		};
@@ -710,9 +716,12 @@ class CommandGroupContainerState {
 			deps: () => this.shouldRender,
 		});
 
-		$effect(() => {
-			return this.root.registerGroup(this.opts.id.current);
-		});
+		watch(
+			() => this.opts.id.current,
+			() => {
+				return this.root.registerGroup(this.opts.id.current);
+			}
+		);
 
 		$effect(() => {
 			if (this.opts.value.current) {
