48f67728752ccb6f43833d4bf59acedf695f98f7
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-20 13:35:47 -0400

    range cal

--- packages/bits-ui/src/lib/bits/range-calendar/components/range-calendar-cell.svelte
+++ packages/bits-ui/src/lib/bits/range-calendar/components/range-calendar-cell.svelte
@@ -2,12 +2,14 @@
 	import { box, mergeProps } from "svelte-toolbelt";
 	import type { RangeCalendarCellProps } from "../types.js";
 	import { useRangeCalendarCell } from "../range-calendar.svelte.js";
-	import { useId } from "$lib/internal/use-id.js";
+	import { createId } from "$lib/internal/create-id.js";
+
+	const uid = $props.id();
 
 	let {
 		children,
 		child,
-		id = useId(),
+		id = createId(uid),
 		ref = $bindable(null),
 		date,
 		month,
