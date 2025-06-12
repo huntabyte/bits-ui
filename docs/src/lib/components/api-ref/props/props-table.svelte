636344450ca1a413d0cd63188f74e4fe33f4db0c
Hunter Johnston <johnstonhuntera@gmail.com>
2025-06-11 21:45:18 -0400

    more

--- docs/src/lib/components/api-ref/props/props-table.svelte
+++ docs/src/lib/components/api-ref/props/props-table.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import type { PropObj } from "$lib/types/index.js";
+	import type { PropObj, PropSchema } from "$lib/types/index.js";
 	import * as Table from "$lib/components/ui/table/index.js";
 	import Code from "$lib/components/markdown/code.svelte";
 	import PropsRequiredBadge from "./props-required-badge.svelte";
 	import PropsBindableBadge from "./props-bindable-badge.svelte";
 	import { parseMarkdown } from "$lib/utils/markdown.js";
 	import PropsTypeContent from "./props-type-content.svelte";
+	import PropsTypeContentMobile from "./props-type-content-mobile.svelte";
 
 	let { props: _props }: { props: PropObj<Record<string, unknown>> } = $props();
 
-	const propData = $derived.by(() => {
+	const propData: Array<PropSchema & { name: string }> = $derived.by(() => {
 		if (!_props) return [];
 		return Object.entries(_props).map(([name, prop]) => {
 			return {
@@ -23,21 +24,21 @@
 <Table.Root>
 	<Table.Header>
 		<Table.Row class="w-1/4">
-			<Table.Head class="w-full whitespace-nowrap pr-1 sm:w-[38%]">Property</Table.Head>
+			<Table.Head class="w-[90%] whitespace-nowrap pr-1 sm:w-[38%]">Property</Table.Head>
 			<Table.Head class="hidden w-[22%] whitespace-nowrap pr-1 sm:table-cell">
 				Type
 			</Table.Head>
 			<Table.Head class="hidden w-[40%] whitespace-nowrap sm:table-cell">
 				Description
 			</Table.Head>
-			<Table.Head class="w-8 sm:hidden">Details</Table.Head>
+			<Table.Head class="w-[10%] sm:hidden">Details</Table.Head>
 		</Table.Row>
 	</Table.Header>
 	<Table.Body>
 		{#each propData as p, i (p.name + i)}
 			<Table.Row>
 				<Table.Cell class="flex items-center gap-1 pr-1 align-baseline">
-					<Code class="text-foreground font-semibold">{p?.name}</Code>
+					<Code class="text-foreground font-semibold">{p.name}</Code>
 					{#if p.required}
 						<PropsRequiredBadge class="hidden sm:block" />
 					{/if}
@@ -46,7 +47,7 @@
 					{/if}
 				</Table.Cell>
 				<Table.Cell class="hidden pr-1 align-baseline sm:table-cell">
-					<PropsTypeContent type={p?.type} />
+					<PropsTypeContent type={p.type} />
 				</Table.Cell>
 				<Table.Cell class="hidden align-baseline sm:table-cell">
 					<p class="text-sm leading-[1.5rem]">
@@ -65,11 +66,7 @@
 					</div>
 				</Table.Cell>
 				<Table.Cell class="text-center align-middle sm:hidden">
-					<!-- <PropMobilePopover
-						type={p?.type}
-						description={p?.description}
-						defaultValue={p?.default}
-					/> -->
+					<PropsTypeContentMobile prop={p} />
 				</Table.Cell>
 			</Table.Row>
 		{/each}
--- docs/src/lib/components/scroll-area.svelte
+++ docs/src/lib/components/scroll-area.svelte
@@ -6,8 +6,13 @@
 	let {
 		class: className,
 		children,
+		scrollbarXProps,
+		scrollbarYProps,
 		...restProps
-	}: ComponentProps<typeof ScrollArea.Root> = $props();
+	}: ComponentProps<typeof ScrollArea.Root> & {
+		scrollbarXProps?: Partial<ComponentProps<typeof ScrollArea.Scrollbar>>;
+		scrollbarYProps?: Partial<ComponentProps<typeof ScrollArea.Scrollbar>>;
+	} = $props();
 </script>
 
 <ScrollArea.Root {...restProps}>
@@ -16,13 +21,21 @@
 	</ScrollArea.Viewport>
 	<ScrollArea.Scrollbar
 		orientation="vertical"
-		class="hover:bg-dark-10 data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out-0 data-[state=visible]:fade-in-0 flex w-2.5 touch-none select-none rounded-full border-l border-l-transparent bg-transparent p-px transition-all duration-200 hover:w-3"
+		{...scrollbarYProps}
+		class={cn(
+			"hover:bg-dark-10 data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out-0 data-[state=visible]:fade-in-0 flex w-2.5 touch-none select-none rounded-full border-l border-l-transparent bg-transparent p-px transition-all duration-200 hover:w-3",
+			scrollbarYProps?.class
+		)}
 	>
 		<ScrollArea.Thumb class="bg-muted-foreground flex-1 rounded-full" />
 	</ScrollArea.Scrollbar>
 	<ScrollArea.Scrollbar
 		orientation="horizontal"
-		class="hover:bg-dark-10 flex h-2.5 touch-none select-none rounded-full border-t border-t-transparent bg-transparent p-px transition-all duration-200 hover:h-3 "
+		{...scrollbarXProps}
+		class={cn(
+			"hover:bg-dark-10 flex h-2.5 touch-none select-none rounded-full border-t border-t-transparent bg-transparent p-px transition-all duration-200 hover:h-3 ",
+			scrollbarXProps?.class
+		)}
 	>
 		<ScrollArea.Thumb class="bg-muted-foreground rounded-full" />
 	</ScrollArea.Scrollbar>
