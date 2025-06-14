a46c7b396d30bd4b914caf00709988977a24a37a
Hunter Johnston <johnstonhuntera@gmail.com>
2025-06-13 20:49:25 -0400

    mobile menu updates

--- docs/src/lib/components/navigation/mobile-nav.svelte
+++ docs/src/lib/components/navigation/mobile-nav.svelte
@@ -1,48 +1,68 @@
 <script lang="ts">
-	import SquareHalf from "phosphor-svelte/lib/SquareHalf";
 	import MobileLink from "./mobile-link.svelte";
-	import { Logo } from "$icons/index.js";
 	import { Button } from "$lib/components/ui/button/index.js";
-	import * as Sheet from "$lib/components/ui/sheet/index.js";
 	import { navigation } from "$lib/config/index.js";
+	import { Popover } from "bits-ui";
+	import ScrollArea from "../scroll-area.svelte";
+	import { page } from "$app/state";
+	import MobileMenuIcon from "./mobile-menu-icon.svelte";
 
 	let open = $state(false);
 </script>
 
-<Sheet.Root bind:open>
-	<Sheet.Trigger>
+<Popover.Root bind:open>
+	<Popover.Trigger>
 		{#snippet child({ props })}
 			<Button
 				{...props}
 				variant="ghost"
-				class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+				class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden [&_svg]:size-6"
 			>
-				<SquareHalf class="h-5 w-5" />
-				<span class="sr-only">Toggle Menu</span>
+				<MobileMenuIcon {open} />
+				<span class="sr-only">Toggle menu</span>
 			</Button>
 		{/snippet}
-	</Sheet.Trigger>
-	<Sheet.Content side="left" class="pr-0">
-		<MobileLink href="/" class="flex items-center" onClose={() => (open = false)}>
-			<Logo />
-		</MobileLink>
-		<div class="my-4 h-[calc(100vh-8rem)] overflow-auto pb-10 pl-6">
-			<div class="flex flex-col space-y-2">
-				{#each navigation.sidebar as navItem, index (index)}
-					<div class="flex flex-col space-y-3 pt-6">
-						<h4 class="font-medium">{navItem.title}</h4>
-						{#if navItem?.items?.length}
-							{#each navItem.items as item (item.title + item.href)}
-								{#if !item.disabled && item.href}
-									<MobileLink href={item.href} onClose={() => (open = false)}>
-										{item.title}
-									</MobileLink>
+	</Popover.Trigger>
+	<Popover.Portal>
+		<Popover.Content
+			class="h-(--bits-popover-content-available-height) w-(--bits-popover-content-available-width) bg-background/90 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--bits-popover-content-transform-origin) pr-0 backdrop-blur"
+			align="start"
+			side="bottom"
+			alignOffset={-16}
+			sideOffset={14}
+			preventScroll
+		>
+			<ScrollArea
+				class="h-(--bits-popover-content-available-height) w-(--bits-popover-content-available-width) max-h-none max-w-none"
+			>
+				<div class="flex flex-col gap-2">
+					{#each navigation.sidebar as navItem, index (index)}
+						<div class="flex flex-col pt-3" data-index={index}>
+							<h4
+								class="text-muted-foreground mb-2 px-5 text-sm font-medium uppercase"
+							>
+								{navItem.title}
+							</h4>
+							<div class="flex flex-col">
+								{#if navItem?.items?.length}
+									{#each navItem.items as item (item.title + item.href)}
+										{#if !item.disabled && item.href}
+											<MobileLink
+												href={item.href}
+												data-active={item.href === page.url.pathname}
+												onClose={() => (open = false)}
+												class="text-foreground/95 data-[active=true]:text-accent px-5 py-1.5 text-[22px] font-medium"
+											>
+												{item.title}
+											</MobileLink>
+										{/if}
+									{/each}
 								{/if}
-							{/each}
-						{/if}
-					</div>
-				{/each}
-			</div>
-		</div>
-	</Sheet.Content>
-</Sheet.Root>
+							</div>
+						</div>
+					{/each}
+				</div>
+			</ScrollArea>
+		</Popover.Content>
+	</Popover.Portal>
+</Popover.Root>
