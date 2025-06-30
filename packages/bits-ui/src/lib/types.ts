a65c4682546f02d154b08ed37c5408f0555fde15
Hunter Johnston <johnstonhuntera@gmail.com>
2025-06-29 21:18:33 -0400

    remove duplicate exports

--- packages/bits-ui/src/lib/types.ts
+++ packages/bits-ui/src/lib/types.ts
@@ -17,7 +17,6 @@ export type * from "$lib/bits/dialog/types.js";
 export type * from "$lib/bits/dropdown-menu/types.js";
 export type * from "$lib/bits/label/types.js";
 export type * from "$lib/bits/link-preview/types.js";
-export type * from "$lib/bits/select/types.js";
 export type * from "$lib/bits/menubar/types.js";
 export type * from "$lib/bits/meter/types.js";
 export type * from "$lib/bits/navigation-menu/types.js";
