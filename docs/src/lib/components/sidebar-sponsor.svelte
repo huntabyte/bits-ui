e6e632bf3b53d137c7225179e51d871f450290d1
Hunter Johnston <johnstonhuntera@gmail.com>
2026-02-12 16:12:50 -0500

    init

--- docs/src/lib/components/sidebar-sponsor.svelte
+++ docs/src/lib/components/sidebar-sponsor.svelte
@@ -1,11 +1,15 @@
-<div class="rounded-input flex flex-col border-2 border-dashed p-2">
+<a
+	class="rounded-input flex flex-col border-2 border-dashed p-2"
+	href="https://github.com/EpicenterHQ/epicenter"
+>
 	<div class="flex h-[150px] flex-col items-center justify-center">
-		<span class="text-foreground/90 text-center text-xs font-medium">Your Logo Here</span>
+		<img src="/logo/epicenter.svg" alt="Epicenter" class="h-full w-full" />
+		<!-- <span class="text-foreground/90 text-center text-xs font-medium">Your Logo Here</span>
 		<p class="text-muted-foreground mt-2 text-center text-xs leading-relaxed">
 			Support the project and reach thousands of developers.
 		</p>
 		<a href="mailto:sponsorship@huntabyte.com" class="link group mt-2 text-xs">
 			<span>Get in touch</span>
-		</a>
+		</a> -->
 	</div>
-</div>
+</a>
