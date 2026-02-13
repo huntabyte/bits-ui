a0f0c38af9ee9b621ddb59dc01e85349ab71e690
Hunter Johnston <johnstonhuntera@gmail.com>
2026-02-12 22:05:35 -0500

    wip

--- docs/src/lib/components/sidebar-sponsor.svelte
+++ docs/src/lib/components/sidebar-sponsor.svelte
@@ -1,15 +1,43 @@
 <a
+	href="https://github.com/EpicenterHQ/epicenter"
+	class="bg-background border-foreground/5 hover:border-foreground/10 group relative block h-[150px] w-full overflow-hidden rounded-xl border transition-all"
+>
+	<!-- Glow -->
+	<div
+		class="absolute -right-12 -top-12 h-32 w-32 rounded-full bg-[#FFD4B2] opacity-10 blur-[50px] transition-opacity group-hover:opacity-20"
+	></div>
+	<div
+		class="absolute -bottom-12 -left-12 h-32 w-32 rounded-full bg-[#CBA6F7] opacity-10 blur-[50px] transition-opacity group-hover:opacity-20"
+	></div>
+
+	<div class="relative z-10 p-3.5">
+		<div class="mb-8 flex items-center justify-between">
+			<span class="text-muted-foreground text-[10px] font-medium uppercase tracking-wide"
+				>Special Sponsor</span
+			>
+			<div class="size-10 opacity-80 transition-opacity group-hover:opacity-100">
+				<svg viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="170" cy="170" r="100" fill="#cccccc" opacity="1"/>
+					<circle cx="230" cy="230" r="100" fill="#ffffff" opacity="1"/>
+				  </svg>
+			</div>
+		</div>
+		<div class="space-y-1">
+			<h3 class="text-foreground text-lg font-semibold tracking-tight">Epicenter</h3>
+			<p class="text-xs leading-relaxed text-foreground/85">
+				Open source, local first apps
+
+			</p>
+		</div>
+	</div>
+</a>
+
+<!-- <a
 	class="rounded-input flex flex-col border-2 border-dashed p-2"
 	href="https://github.com/EpicenterHQ/epicenter"
 >
 	<div class="flex h-[150px] flex-col items-center justify-center">
-		<img src="/logo/epicenter.svg" alt="Epicenter" class="h-full w-full" />
-		<!-- <span class="text-foreground/90 text-center text-xs font-medium">Your Logo Here</span>
-		<p class="text-muted-foreground mt-2 text-center text-xs leading-relaxed">
-			Support the project and reach thousands of developers.
-		</p>
-		<a href="mailto:sponsorship@huntabyte.com" class="link group mt-2 text-xs">
-			<span>Get in touch</span>
-		</a> -->
+		
+		
 	</div>
-</a>
+</a> -->
