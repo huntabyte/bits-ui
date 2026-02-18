2b66d9c3fb436538faa93aaa121fe63b5f2e7ee7
Hunter Johnston <johnstonhuntera@gmail.com>
2026-02-18 10:41:26 -0500

    docs: update special sponsor

--- docs/src/lib/components/sidebar-sponsor.svelte
+++ docs/src/lib/components/sidebar-sponsor.svelte
@@ -1,6 +1,6 @@
 <a
 	href="https://github.com/EpicenterHQ/epicenter"
-	class="bg-background border-foreground/5 hover:border-foreground/10 group relative block h-[150px] w-full overflow-hidden rounded-xl border transition-all"
+	class="bg-muted/40 dark:bg-background border-foreground/5 hover:border-foreground/10 group relative block h-[150px] w-full select-none overflow-hidden rounded-xl border transition-all"
 >
 	<div
 		class="absolute -right-12 -top-12 h-32 w-32 rounded-full bg-[#FFD4B2] opacity-10 blur-[50px] transition-opacity group-hover:opacity-20"
@@ -16,27 +16,14 @@
 			>
 			<div class="size-10 opacity-80 transition-opacity group-hover:opacity-100">
 				<svg viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
-					<circle cx="170" cy="170" r="100" fill="#cccccc" opacity="1"/>
-					<circle cx="230" cy="230" r="100" fill="#ffffff" opacity="1"/>
-				  </svg>
+					<circle cx="170" cy="170" r="100" fill="#cccccc" opacity="1" />
+					<circle cx="230" cy="230" r="100" fill="#ffffff" opacity="1" />
+				</svg>
 			</div>
 		</div>
 		<div class="space-y-1">
 			<h3 class="text-foreground text-lg font-semibold tracking-tight">Epicenter</h3>
-			<p class="text-xs leading-relaxed text-foreground/85">
-				Open source, local first apps
-
-			</p>
+			<p class="text-foreground/85 text-xs leading-relaxed">Open source, local first apps</p>
 		</div>
 	</div>
 </a>
-
-<!-- <a
-	class="rounded-input flex flex-col border-2 border-dashed p-2"
-	href="https://github.com/EpicenterHQ/epicenter"
->
-	<div class="flex h-[150px] flex-col items-center justify-center">
-		
-		
-	</div>
-</a> -->
